0a0c9e7983c7dcb7a4b25751b877008ce372da66
Brent Shepherd <thenbrent@gmail.com>
2014-11-22 16:41:37 -0800

    Add WC_Subscription::__get()
    Part of #5

--- includes/class-wc-subscription.php
+++ includes/class-wc-subscription.php
@@ -14,6 +14,12 @@
 
 class WC_Subscription extends WC_Order {
 
+	/** @protected WC_Order Stores order data for the order in which the subscription was purchased (if any) */
+	protected $order;
+
+	/** @protected Object Stores an instance of the WC_Payment_Gateway used to process recurring payments (if any) */
+	protected $payment_gateway = null;
+
 	/**
 	 * Initialize the subscription object.
 	 *
@@ -33,6 +39,45 @@ class WC_Subscription extends WC_Order {
 	 */
 	public function populate( $result ) {
 		parent::populate( $result );
+	}
+
+	/**
+	 * __get function.
+	 *
+	 * @param mixed $key
+	 * @return mixed
+	 */
+	public function __get( $key ) {
+
+		if ( in_array( $key, array( 'start_date', 'trial_end_date', 'next_payment_date', 'end_date', 'last_payment_date' ) ) ) {
+
+			$value = $this->get_date( $key );
+
+		} elseif ( 'order' == $key ) {
+
+			if ( $this->post->post_parent > 0 ) {
+				$this->order = wc_get_order( $this->post->post_parent );
+			} else {
+				$this->order = null;
+			}
+
+		} elseif ( 'payment_gateway' == $key ) {
+
+			// Only set the payment gateway once and only when we first need it
+			if ( ! isset( $this->payment_gateway ) ) {
+				$payment_gateways = WC()->payment_gateways->payment_gateways();
+				$this->payment_gateway = isset( $payment_gateways[ $this->payment_method ] ) ? $payment_gateways[ $this->payment_method ] : null;
+			}
+
+			$value = $this->payment_gateway;
+
+		} else {
+
+			$value = parent::__get( $key );
+
+		}
+
+		return $value;
 	}
 
 }
