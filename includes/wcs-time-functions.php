bc983d3f8fc91fff654903d0dcdb704f047a0935
Brent Shepherd <thenbrent@gmail.com>
2014-11-24 11:54:57 -0800

    Add empty time functions file
    Part of #10

---
+++ includes/wcs-time-functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * WooCommerce Time Functions
+ *
+ * Functions for time values and ranges
+ *
+ * @author 		Prospress
+ * @category 	Core
+ * @package 	WooCommerce Subscriptions/Functions
+ * @version     2.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
