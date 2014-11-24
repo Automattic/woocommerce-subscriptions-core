d382e38557f255b511fa18d8d33da7479a3eafe3
Brent Shepherd <thenbrent@gmail.com>
2014-11-24 12:29:00 -0800

    Add wcs_add_months()
    And deprecate `WC_Subscriptions::add_months()`
    
    Part of #10

--- includes/wcs-time-functions.php
+++ includes/wcs-time-functions.php
@@ -194,3 +194,34 @@ function wcs_get_subscription_trial_lengths( $subscription_period = '' ) {
 	}
 }
 
+/**
+ * Workaround the last day of month quirk in PHP's strtotime function.
+ *
+ * Adding +1 month to the last day of the month can yield unexpected results with strtotime().
+ * For example:
+ * - 30 Jan 2013 + 1 month = 3rd March 2013
+ * - 28 Feb 2013 + 1 month = 28th March 2013
+ *
+ * What humans usually want is for the date to continue on the last day of the month.
+ *
+ * @param int A Unix timestamp to add the months too.
+ * @param int The number of months to add to the timestamp.
+ * @since 2.0
+ */
+function wcs_add_months( $from_timestamp, $months_to_add ) {
+
+	$first_day_of_month = date( 'Y-m', $from_timestamp ) . '-1';
+	$days_in_next_month = date( 't', strtotime( "+ {$months_to_add} month", strtotime( $first_day_of_month ) ) );
+
+	// Payment is on the last day of the month OR number of days in next billing month is less than the the day of this month (i.e. current billing date is 30th January, next billing date can't be 30th February)
+	if ( date( 'd m Y', $from_timestamp ) === date( 't m Y', $from_timestamp ) || date( 'd', $from_timestamp ) > $days_in_next_month ) {
+		for ( $i = 1; $i <= $months_to_add; $i++ ) {
+			$next_month = strtotime( '+ 3 days', $from_timestamp ); // Add 3 days to make sure we get to the next month, even when it's the 29th day of a month with 31 days
+			$next_timestamp = $from_timestamp = strtotime( date( 'Y-m-t H:i:s', $next_month ) ); // NB the "t" to get last day of next month
+		}
+	} else { // Safe to just add a month
+		$next_timestamp = strtotime( "+ {$months_to_add} month", $from_timestamp );
+	}
+
+	return $next_timestamp;
+}
