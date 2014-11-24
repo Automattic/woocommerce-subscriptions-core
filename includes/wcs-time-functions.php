47c64a1e2101123e4a7a07ac6fa06daad7803c23
Brent Shepherd <thenbrent@gmail.com>
2014-11-24 12:19:09 -0800

    Add wcs_get_subscription_ranges()
    And deprecate `WC_Subscriptions_Manager::get_subscription_ranges()`
    
    Part of #10

--- includes/wcs-time-functions.php
+++ includes/wcs-time-functions.php
@@ -57,3 +57,74 @@ function wcs_get_subscription_trial_period_strings( $number = 1, $period = '' ) 
 	return ( ! empty( $period ) ) ? $translated_periods[ $period ] : $translated_periods;
 }
 
+/**
+ * Returns an array of subscription lengths.
+ *
+ * PayPal Standard Allowable Ranges
+ * D – for days; allowable range is 1 to 90
+ * W – for weeks; allowable range is 1 to 52
+ * M – for months; allowable range is 1 to 24
+ * Y – for years; allowable range is 1 to 5
+ *
+ * @param string (optional) One of day, week, month or year. If empty, all subscription ranges are returned.
+ * @since 2.0
+ */
+function wcs_get_subscription_ranges( $subscription_period = '' ) {
+
+	$subscription_periods = wcs_get_subscription_period_strings();
+
+	foreach ( array( 'day', 'week', 'month', 'year' ) as $period ) {
+
+		$subscription_lengths = array(
+			__( 'all time', 'woocommerce-subscriptions' ),
+		);
+
+		switch( $period ) {
+			case 'day':
+				$subscription_lengths[] = __( '1 day', 'woocommerce-subscriptions' );
+				break;
+			case 'week':
+				$subscription_lengths[] = __( '1 week', 'woocommerce-subscriptions' );
+				break;
+			case 'month':
+				$subscription_lengths[] = __( '1 month', 'woocommerce-subscriptions' );
+				break;
+			case 'year':
+				$subscription_lengths[] = __( '1 year', 'woocommerce-subscriptions' );
+				break;
+		}
+
+		switch( $period ) {
+			case 'day':
+				$subscription_range = range( 2, 90 );
+				break;
+			case 'week':
+				$subscription_range = range( 2, 52 );
+				break;
+			case 'month':
+				$subscription_range = range( 2, 24 );
+				break;
+			case 'year':
+				$subscription_range = range( 2, 5 );
+				break;
+		}
+
+		foreach ( $subscription_range as $number ) {
+			$subscription_range[ $number ] = wcs_get_subscription_period_strings( $number, $period );
+		}
+
+		// Add the possible range to all time range
+		$subscription_lengths += $subscription_range;
+
+		$subscription_ranges[ $period ] = $subscription_lengths;
+	}
+
+	$subscription_ranges = apply_filters( 'woocommerce_subscription_lengths', $subscription_ranges, $subscription_period );
+
+	if ( ! empty( $subscription_period ) ) {
+		return $subscription_ranges[ $subscription_period ];
+	} else {
+		return $subscription_ranges;
+	}
+}
+
