bf94368e8fec5938edaf4e7c183c3617cb9a54b8
Brent Shepherd <thenbrent@gmail.com>
2014-11-24 11:59:27 -0800

    Add wcs_get_subscription_period_strings()
    And deprecate `WC_Subscriptions_Manager::get_subscription_period_strings()`
    
    Part of #10

--- includes/wcs-time-functions.php
+++ includes/wcs-time-functions.php
@@ -13,3 +13,26 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+
+
+/**
+ * Return an i18n'ified associative array of all possible subscription periods.
+ *
+ * @param int (optional) An interval in the range 1-6
+ * @param string (optional) One of day, week, month or year. If empty, all subscription ranges are returned.
+ * @since 2.0
+ */
+function wcs_get_subscription_period_strings( $number = 1, $period = '' ) {
+
+	$translated_periods = apply_filters( 'woocommerce_subscription_periods',
+		array(
+			'day'   => sprintf( _n( 'day', '%s days', $number, 'woocommerce-subscriptions' ), $number ),
+			'week'  => sprintf( _n( 'week', '%s weeks', $number, 'woocommerce-subscriptions' ), $number ),
+			'month' => sprintf( _n( 'month', '%s months', $number, 'woocommerce-subscriptions' ), $number ),
+			'year'  => sprintf( _n( 'year', '%s years', $number, 'woocommerce-subscriptions' ), $number )
+		)
+	);
+
+	return ( ! empty( $period ) ) ? $translated_periods[ $period ] : $translated_periods;
+}
+
