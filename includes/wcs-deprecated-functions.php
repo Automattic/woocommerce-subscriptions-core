c2eca4a28c11813eb3301a0f547f8f993466e856
Brent Shepherd <thenbrent@gmail.com>
2014-11-26 00:47:50 -0800

    Initial commit of wcs-deprecated-functions.php
    Part of #17

---
+++ includes/wcs-deprecated-functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * WooCommerce Subscriptions Deprecated Functions
+ *
+ * Functions for handling backward compatibility with the Subscription 1.n
+ * data structure and reference system (i.e. $subscription_key instead of a
+ * post ID)
+ *
+ * @author 		Prospress
+ * @category 	Core
+ * @package 	WooCommerce Subscriptions/Functions
+ * @version     2.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
