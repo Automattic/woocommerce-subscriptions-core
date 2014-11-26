2f6c80c62aa0cdc68bcf4da709999583d9312d88
Brent Shepherd <thenbrent@gmail.com>
2014-11-25 23:23:51 -0800

    Initial commit of user functions

---
+++ includes/wcs-user-functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * WooCommerce User Functions
+ *
+ * Functions for managing user
+ *
+ * @author 		Prospress
+ * @category 	Core
+ * @package 	WooCommerce Subscriptions/Functions
+ * @version     2.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
