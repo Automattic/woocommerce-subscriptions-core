2228dfa307d2c2e11461250998547b060a158650
Brent Shepherd <thenbrent@gmail.com>
2014-12-09 18:30:15 -0800

    Initial commit wcs_get_subscriptions_for_order()
    A convenience wrapper for `wcs_get_susbcriptions()` that will be used
    frequently in `WC_Subscriptions_Order` methods
    
    Loosely part of #31

--- includes/wcs-order-functions.php
+++ includes/wcs-order-functions.php
@@ -11,3 +11,19 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+
+/**
+ * A wrapper for @see wcs_get_subscriptions() which accepts simply an order ID
+ *
+ * @param int|WC_Order $order_id The post_id of a shop_order post or an intsance of a WC_Order object
+ * @return array Subscription details in post_id => WC_Subscription form.
+ * @since  2.0
+ */
+function wcs_get_subscriptions_for_order( $order_id ) {
+
+	if ( is_object( $order_id ) ) {
+		$order_id = $order_id->id;
+	}
+
+	return wcs_get_subscriptions( array( 'order_id' => $order_id ) );;
+}
