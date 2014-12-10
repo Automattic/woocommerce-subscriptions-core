2ae25ad32c77e56b6c3d188258778865b55a1bb9
Brent Shepherd <thenbrent@gmail.com>
2014-12-09 18:26:56 -0800

    Initial commit of wcs-order-functions.php

---
+++ includes/wcs-order-functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * WooCommerce Subscriptions Order Functions
+ *
+ * @author 		Prospress
+ * @category 	Core
+ * @package 	WooCommerce Subscriptions/Functions
+ * @version     2.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
