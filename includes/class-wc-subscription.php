15acf51fe7b4178ee643195f9d5cdfc9fc7728fb
thenbrent <thenbrent@gmail.com>
2014-12-18 13:17:50 +1000

    Use human time diff for dates within a week
    For example:
    * 3 days ago
    * 1 hour ago

--- includes/class-wc-subscription.php
+++ includes/class-wc-subscription.php
@@ -556,6 +556,8 @@ class WC_Subscription extends WC_Order {
 
 			if ( $time_diff > 0 && $time_diff < WEEK_IN_SECONDS ) {
 				$date_to_display = sprintf( __( 'In %s', 'woocommerce-subscriptions' ), human_time_diff( current_time( 'timestamp', true ), $timestamp_gmt ) );
+			} elseif ( $time_diff < 0 && absint( $time_diff ) < WEEK_IN_SECONDS ) {
+				$date_to_display = sprintf( __( '%s ago', 'woocommerce-subscriptions' ), human_time_diff( current_time( 'timestamp', true ), $timestamp_gmt ) );
 			} else {
 				$date_to_display = date_i18n( wc_date_format(), $this->get_time( $date_type, 'site' ) );
 			}
