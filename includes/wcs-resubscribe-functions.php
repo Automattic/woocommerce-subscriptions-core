36c46b2d413cf99473fdf623291c967695031bf7
Brent Shepherd <thenbrent@gmail.com>
2015-02-17 15:39:39 -0800

    Add wcs_is_resubscribe_order() function
    Part of #32

--- includes/wcs-resubscribe-functions.php
+++ includes/wcs-resubscribe-functions.php
@@ -13,3 +13,24 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+
+/**
+ * Check if a given order was created to resubscribe to a cancelled or expired subscription.
+ *
+ * @param WC_Order|int $order The WC_Order object or ID of a WC_Order order.
+ * @since 2.0
+ */
+function wcs_is_resubscribe_order( $order ) {
+
+	if ( ! is_object( $order ) ) {
+		$order = new WC_Order( $order );
+	}
+
+	if ( '' !== get_post_meta( $order->id, '_original_subscription', true ) && 0 == $order->post->post_parent ) {
+		$is_resubscribe_order = true;
+	} else {
+		$is_resubscribe_order = false;
+	}
+
+	return apply_filters( 'woocommerce_subscriptions_is_resubscribe_order', $is_resubscribe_order, $order );
+}
