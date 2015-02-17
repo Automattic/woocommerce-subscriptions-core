042d4b23519ee562ca9330385fc98e8bc23d18b6
Brent Shepherd <thenbrent@gmail.com>
2015-02-17 15:33:40 -0800

    Add resubscribe functions file

---
+++ includes/wcs-resubscribe-functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * WooCommerce Subscriptions Resubscribe Functions
+ *
+ * Functions for managing resubscribing to expired or cancelled subscriptions.
+ *
+ * @author 		Prospress
+ * @category 	Core
+ * @package 	WooCommerce Subscriptions/Functions
+ * @version     2.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
