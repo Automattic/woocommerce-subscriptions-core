b6093f8a1ba34af4e4d175c295da324f4e8045d6
Brent Shepherd <thenbrent@gmail.com>
2015-02-18 14:33:07 -0800

    Move is_purchasable() to new classes
    * Add `WCS_Cart_Resubscribe::is_purchasable()`
    * Add `WCS_Cart_Renewal::is_purchasable()`
    * Deprecate `WC_Subscriptions_Renewal_Order::is_purchasable()`
    
    Part of #32

--- includes/class-wcs-cart-renewal.php
+++ includes/class-wcs-cart-renewal.php
@@ -14,12 +14,51 @@
 
 class WCS_Cart_Renewal {
 
+	/* The flag used to indicate if a cart item is a renewal */
+	public $cart_item_key = 'subscription_renewal';
+
 	/**
 	 * Bootstraps the class and hooks required actions & filters.
 	 *
 	 * @since 2.0
 	 */
 	public function __construct() {
+		$this->setup_hooks();
+	}
+
+	/**
+	 * Bootstraps the class and hooks required actions & filters.
+	 *
+	 * @since 2.0
+	 */
+	public function setup_hooks() {
+
+		// Allow renewal of limited subscriptions
+		add_filter( 'woocommerce_subscription_is_purchasable', array( &$this, 'is_purchasable' ), 12, 2 );
+		add_filter( 'woocommerce_subscription_variation_is_purchasable', array( &$this, 'is_purchasable' ), 12, 2 );
+	}
+
+	/**
+	 * If a product is being marked as not purchasable because it is limited and the customer has a subscription,
+	 * but the current request is to resubscribe to the subscription, then mark it as purchasable.
+	 *
+	 * @since 2.0
+	 * @return bool
+	 */
+	public function is_purchasable( $is_purchasable, $product ) {
+
+		// If the product is being set as not-purchasable by Subscriptions (due to limiting)
+		if ( false === $is_purchasable && false === WC_Subscriptions_Product::is_purchasable( $is_purchasable, $product ) ) {
+
+			// Adding to cart from the product page
+			if ( isset( $_GET[ $this->cart_item_key ] ) ) {
+
+				$is_purchasable = true;
+
+			}
+		}
+
+		return $is_purchasable;
 	}
 
 }
--- includes/class-wcs-cart-resubscribe.php
+++ includes/class-wcs-cart-resubscribe.php
@@ -15,13 +15,49 @@
 
 class WCS_Cart_Resubscribe extends WCS_Cart_Renewal {
 
+	/* The flag used to indicate if a cart item is a renewal */
+	public $cart_item_key = 'subscription_resubscribe';
+
 	/**
 	 * Bootstraps the class and hooks required actions & filters.
 	 *
 	 * @since 2.0
 	 */
 	public function __construct() {
+		$this->setup_hooks();
 	}
 
+	/**
+	 * If a product is being marked as not purchasable because it is limited and the customer has a subscription,
+	 * but the current request is to resubscribe to the subscription, then mark it as purchasable.
+	 *
+	 * @since 2.0
+	 * @return bool
+	 */
+	public function is_purchasable( $is_purchasable, $product ) {
+
+		// If the product is being set as not-purchasable by Subscriptions (due to limiting)
+		if ( false === $is_purchasable && false === WC_Subscriptions_Product::is_purchasable( $is_purchasable, $product ) ) {
+
+			// Validating when restoring cart from session
+			if ( false !== wcs_cart_contains_resubscribe() ) {
+
+				$is_purchasable = true;
+
+			// Restoring cart from session, so need to check the cart in the session (wcs_cart_contains_renewal() only checks the cart)
+			} elseif ( isset( WC()->session->cart ) ) {
+
+				foreach ( WC()->session->cart as $cart_item_key => $cart_item ) {
+					if ( $product->id == $cart_item['product_id'] && isset( $cart_item[ $this->cart_item_key ] ) ) {
+						$is_purchasable = true;
+						break;
+					}
+				}
+
+			}
+		}
+
+		return $is_purchasable;
+	}
 }
 new WCS_Cart_Resubscribe();
