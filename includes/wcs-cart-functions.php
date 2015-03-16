89a46a1c520ded895c7dbecdded13896a1463601
thenbrent <thenbrent@gmail.com>
2015-03-15 18:11:02 -0700

    Add wcs_cart_pluck()
    For use with #157

--- includes/wcs-cart-functions.php
+++ includes/wcs-cart-functions.php
@@ -14,3 +14,32 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+
+/**
+ * Return a given piece of meta data from the cart
+ *
+ * The data can exist on the cart object, a cart item, or product data on a cart item.
+ * The first piece of data with a matching key (in that order) will be returned if it
+ * is found, otherwise, the value specified with $default, will be returned.
+ *
+ * @access public
+ * @return string
+ */
+function wcs_cart_pluck( $cart, $field, $default = 0 ) {
+
+	$value = $default;
+
+	if ( isset( $cart->$field ) ) {
+		$value = $cart->$field;
+	} else {
+		foreach( $cart->get_cart() as $cart_item ) {
+			if ( isset( $cart_item[ $field ] ) ) {
+				$value = $cart_item[ $field ];
+			} elseif ( $cart_item['data']->$field ) {
+				$value = $cart_item['data']->$field;
+			}
+		}
+	}
+
+	return $value;
+}
