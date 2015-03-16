691c629e9bd00dc16de82b505417d548c054a4cd
thenbrent <thenbrent@gmail.com>
2015-03-15 18:09:18 -0700

    Add wcs-cart-functions.php file
    For storing display functions relating to #157

---
+++ includes/wcs-cart-functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * WooCommerce Subscriptions Cart Functions
+ *
+ * Functions for cart specific things, based on wc-cart-functions.php but overloaded
+ * for use with recurring carts.
+ *
+ * @author 		WooThemes
+ * @category 	Core
+ * @package 	WooCommerce/Functions
+ * @version     2.1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
