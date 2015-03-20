1c9fd51af4824d24319b83133e30e6869b857ebd
Brent Shepherd <thenbrent@gmail.com>
2015-03-20 13:17:12 -0700

    Add WCS_Query
    To save a few lines of code by extending `WC_Query`.
    
    Part of #122

--- includes/class-wcs-template-loader.php
+++ includes/class-wcs-template-loader.php
@@ -8,15 +8,7 @@
 class WCS_Template_Loader {
 
 	public static function init() {
-
-		add_action( 'init', __CLASS__ . '::wcs_add_view_subscription_endpoint' );
-
-		add_action( 'query_vars', __CLASS__ . '::wcs_add_query_vars' );
-
 		add_filter( 'wc_get_template', __CLASS__ . '::wcs_add_view_subscription_template', 10, 5 );
-
-		add_filter( 'the_title', __CLASS__ . '::wcs_subscription_endpoint_title', 11, 1 );
-
 	}
 
 	/**
@@ -37,48 +29,6 @@ class WCS_Template_Loader {
 		}
 
 		return $located;
-	}
-
-	/**
-	 * Set the subscription page title when viewing a subscription.
-	 *
-	 * @since 2.0
-	 * @param $title
-	 */
-	public static function wcs_subscription_endpoint_title( $title ) {
-		global $wp;
-
-		if ( is_main_query() && in_the_loop() && is_page() && isset( $wp->query_vars['view-subscription'] ) ) {
-
-			$subscription_id = $wp->query_vars['view-subscription'];
-
-			if ( 'shop_subscription' == get_post_type( $subscription_id ) ) {
-				//remove_filter( 'the_title', __CLASS__ . '::wcs_subscription_endpoint_title' );
-				$title = sprintf( __( 'Subscription #%s', 'woocommerce-subscriptions' ), $subscription_id );
-			}
-
-		}
-
-		return $title;
-	}
-
-	/**
-	 *
-	 *
-	 * @since 2.0
-	 */
-	public static function wcs_add_query_vars( $vars ) {
-		$vars[] = 'view-subscription';
-		return $vars;
-	}
-
-	/**
-	 *
-	 *
-	 * @since 2.0
-	 */
-	public static function wcs_add_view_subscription_endpoint() {
-		add_rewrite_endpoint( 'view-subscription', EP_ROOT | EP_PAGES );
 	}
 }
 WCS_Template_Loader::init();
