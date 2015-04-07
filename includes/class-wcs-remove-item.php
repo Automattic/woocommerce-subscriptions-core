86ecbaa27c950b77a4d84ac097a73e1b17daad15
matttallan <matt.d.a@live.com>
2015-04-07 15:57:33 +1000

    Get Remove url and undo remove urls
    For #175

--- includes/class-wcs-remove-item.php
+++ includes/class-wcs-remove-item.php
@@ -19,5 +19,38 @@ class WCS_Remove_Item {
 		add_action( 'init', __CLASS__ . '::maybe_remove_or_add_item_to_subscription', 100 );
 	}
 
+	/**
+	 * Returns the link used to remove an item from a subscription
+	 *
+	 * @param $subscription_id
+	 * @param $order_item_id
+	 * @param $return_url
+	 * @since 2.0
+	 */
+	public static function get_remove_url( $subscription_id, $order_item_id ) {
+
+		$remove_link = add_query_arg( array( 'subscription_id' => $subscription_id, 'remove_item' => $order_item_id ) );
+		$remove_link = wp_nonce_url( $remove_link, $subscription_id );
+
+		return $remove_link;
+	}
+
+	/**
+	 * Returns the link to undo removing an item from a subscription
+	 *
+	 * @param $subscription_id
+	 * @param $order_item_id
+	 * @param $base_url
+	 * @since 2.0
+	 */
+	public static function get_undo_remove_url( $subscription_id, $order_item_id, $base_url ) {
+
+		$undo_link = add_query_arg( array( 'subscription_id' => $subscription_id, 'undo_remove_item' => $order_item_id ), $base_url );
+		$undo_link = wp_nonce_url( $undo_link, $subscription_id );
+
+		return $undo_link;
+	}
+
+
 }
 WCS_Remove_Item::init();
