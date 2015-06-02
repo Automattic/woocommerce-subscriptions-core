40ca65fc8e97fed38c5d55f5885693b5385724df
Brent Shepherd <thenbrent@gmail.com>
2015-06-02 15:44:16 -0700

    Move v1.3 database upgrade into separate file

--- includes/upgrades/class-wc-subscriptions-upgrader.php
+++ includes/upgrades/class-wc-subscriptions-upgrader.php
@@ -373,23 +373,7 @@ class WC_Subscriptions_Upgrader {
 	 * @since 1.3
 	 */
 	private static function upgrade_database_to_1_3() {
-		global $wpdb;
-
-		// Change transient timeout entries to be a vanilla option
-		$wpdb->query( " UPDATE $wpdb->options
-						SET option_name = TRIM(LEADING '_transient_timeout_' FROM option_name)
-						WHERE option_name LIKE '_transient_timeout_wcs_blocker_%'" );
-
-		// Change transient keys from the < 1.1.5 format to new format
-		$wpdb->query( " UPDATE $wpdb->options
-						SET option_name = CONCAT('wcs_blocker_', TRIM(LEADING '_transient_timeout_block_scheduled_subscription_payments_' FROM option_name))
-						WHERE option_name LIKE '_transient_timeout_block_scheduled_subscription_payments_%'" );
-
-		// Delete old transient values
-		$wpdb->query( " DELETE FROM $wpdb->options
-						WHERE option_name LIKE '_transient_wcs_blocker_%'
-						OR option_name LIKE '_transient_block_scheduled_subscription_payments_%'" );
-
+		include_once( 'upgrade-1-3.php' );
 	}
 
 	/**
