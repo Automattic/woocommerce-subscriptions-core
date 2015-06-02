24b08cfe0138ce221c41be038232363371bb21c0
Brent Shepherd <thenbrent@gmail.com>
2015-06-02 13:48:32 -0700

    Move update helper template into separate file
    Part of #128

--- includes/upgrades/class-wc-subscriptions-upgrader.php
+++ includes/upgrades/class-wc-subscriptions-upgrader.php
@@ -816,12 +816,13 @@ class WC_Subscriptions_Upgrader {
 	 * @since 1.2
 	 */
 	public static function display_database_upgrade_helper() {
+
 		wp_register_style( 'wcs-upgrade', plugins_url( '/css/wcs-upgrade.css', WC_Subscriptions::$plugin_file ) );
 		wp_register_script( 'wcs-upgrade', plugins_url( '/js/wcs-upgrade.js', WC_Subscriptions::$plugin_file ), 'jquery' );
 
 		$script_data = array(
 			'really_old_version' => ( version_compare( self::$active_version, '1.4', '<' ) ) ? 'true' : 'false',
-			'hooks_per_request' => self::$upgrade_limit,
+			'hooks_per_request' => self::$upgrade_limit_hooks,
 			'ajax_url' => admin_url( 'admin-ajax.php' ),
 		);
 
@@ -833,51 +834,10 @@ class WC_Subscriptions_Upgrader {
 			$estimated_duration = ceil( $subscription_count / 500 );
 		}
 
-		@header( 'Content-Type: ' . get_option( 'html_type' ) . '; charset=' . get_option( 'blog_charset' ) ); ?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-<head>
-	<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php esc_attr_e( get_option( 'blog_charset' ) ); ?>" />
-	<title><?php esc_html_e( 'WooCommerce Subscriptions Update', 'woocommerce-subscriptions' ); ?></title>
-	<?php wp_admin_css( 'install', true ); ?>
-	<?php wp_admin_css( 'ie', true ); ?>
-	<?php wp_print_styles( 'wcs-upgrade' ); ?>
-	<?php wp_print_scripts( 'jquery' ); ?>
-	<?php wp_print_scripts( 'wcs-upgrade' ); ?>
-</head>
-<body class="wp-core-ui">
-<h1 id="logo"><img alt="WooCommerce Subscriptions" width="325px" height="120px" src="<?php echo esc_url( plugins_url( 'images/woocommerce_subscriptions_logo.png', WC_Subscriptions::$plugin_file ) ); ?>" /></h1>
-<div id="update-welcome">
-	<h2><?php esc_html_e( 'Database Update Required', 'woocommerce-subscriptions' ); ?></h2>
-	<p><?php esc_html_e( 'The WooCommerce Subscriptions plugin has been updated!', 'woocommerce-subscriptions' ); ?></p>
-	<p><?php esc_html_e( 'Before we send you on your way, we need to update your database to the newest version. If you do not have a recent backup of your site, now is a good time to create one.', 'woocommerce-subscriptions' ); ?></p>
-	<p><?php esc_html_e( 'The update process may take a little while, so please be patient.', 'woocommerce-subscriptions' ); ?></p>
-	<form id="subscriptions-upgrade" method="get" action="<?php echo esc_url( admin_url( 'admin.php' ) ); ?>">
-		<input type="submit" class="button" value="<?php esc_attr_e( 'Update Database', 'woocommerce-subscriptions' ); ?>">
-	</form>
-</div>
-<div id="update-messages">
-	<h2><?php esc_html_e( 'Update in Progress', 'woocommerce-subscriptions' ); ?></h2>
-	<?php if ( 'false' == $script_data['really_old_version'] ) : ?>
-	<p><?php printf( esc_html__( 'The full update process for the %s subscriptions on your site will take approximately %s to %s minutes.', 'woocommerce-subscriptions' ), esc_html__( $subscription_count ), esc_html__( $estimated_duration ), esc_html__( $estimated_duration * 2 ) ); ?></p>
-	<?php endif; ?>
-	<p><?php esc_html_e( 'This page will display the results of the process as each batch of subscriptions is updated. No need to refresh or restart the process. Customers and other non-administrative users will continue to be able to browse your site without interuption while the update is in progress.', 'woocommerce-subscriptions' ); ?></p>
-	<ol>
-	</ol>
-	<img id="update-ajax-loader" alt="loading..." width="16px" height="16px" src="<?php echo esc_url( plugins_url( 'images/ajax-loader@2x.gif', WC_Subscriptions::$plugin_file ) ); ?>" />
-</div>
-<div id="update-complete">
-	<h2><?php esc_html_e( 'Update Complete', 'woocommerce-subscriptions' ); ?></h2>
-	<p><?php esc_html_e( 'Your database has been successfully updated!', 'woocommerce-subscriptions' ); ?></p>
-	<p class="step"><a class="button" href="<?php echo esc_url( self::$about_page_url ); ?>"><?php esc_html_e( 'Continue', 'woocommerce-subscriptions' ); ?></a></p>
-</div>
-<div id="update-error">
-	<h2><?php esc_html_e( 'Update Error', 'woocommerce-subscriptions' ); ?></h2>
-	<p><?php esc_html_e( 'There was an error with the update. Please refresh the page and try again.', 'woocommerce-subscriptions' ); ?></p>
-</div>
-</body>
-</html>
-<?php
+		$about_page_url = self::$about_page_url;
+
+		@header( 'Content-Type: ' . get_option( 'html_type' ) . '; charset=' . get_option( 'blog_charset' ) );
+		include_once( 'templates/wcs-upgrade.php' );
 	}
 
 	/**
