0d6bd96a0e66ad65cc40c9fdb030dacda43cdf77
Brent Shepherd <thenbrent@gmail.com>
2015-06-03 11:48:08 -0700

    Move v1.5 upgrades into separate file
    Also adds WCS_Upgrade_1_5 and upgrade-1-5.php

--- includes/upgrades/class-wc-subscriptions-upgrader.php
+++ includes/upgrades/class-wc-subscriptions-upgrader.php
@@ -177,14 +177,20 @@ class WC_Subscriptions_Upgrader {
 				break;
 
 			case 'products':
-				$upgraded_product_count = self::upgrade_products_1_5();
+
+				require_once( 'upgrade-1-5.php' );
+
+				$upgraded_product_count = WCS_Upgrade_1_5::upgrade_products();
 				$results = array(
 					'message' => sprintf( __( 'Marked %s subscription products as "sold individually".', 'woocommerce-subscriptions' ), $upgraded_product_count ),
 				);
 				break;
 
 			case 'hooks':
-				$upgraded_hook_count = self::upgrade_hooks_1_5();
+
+				require_once( 'upgrade-1-5.php' );
+
+				$upgraded_hook_count = WCS_Upgrade_1_5::upgrade_hooks( self::$upgrade_limit_hooks );
 				$results = array(
 					'upgraded_count' => $upgraded_hook_count,
 					'message'        => sprintf( __( 'Migrated %s subscription related hooks to the new scheduler (in {execution_time} seconds).', 'woocommerce-subscriptions' ), $upgraded_hook_count ),
@@ -192,106 +198,17 @@ class WC_Subscriptions_Upgrader {
 				break;
 		}
 
-		if ( isset( $counter ) && $counter < self::$upgrade_limit ) {
+		if ( isset( $upgraded_subscriptions ) && $upgraded_subscriptions < self::$upgrade_limit_subscriptions ) {
 			self::upgrade_complete();
 		}
 
 		delete_transient( 'wc_subscriptions_is_upgrading' );
 
+		WCS_Upgrade_Logger::add( sprintf( 'Completed upgrade step: %s', $_POST['upgrade_step'] ) );
+
 		header( 'Content-Type: application/json; charset=utf-8' );
 		echo json_encode( $results );
 		exit();
-	}
-
-	/**
-	 * Set status to 'sold individually' for all existing subscription products that haven't already been updated.
-	 *
-	 * Subscriptions 1.5 made it possible for a product to be sold individually or in multiple quantities, whereas
-	 * previously it was possible only to buy a subscription product in a single quantity.
-	 *
-	 * @since 2.0
-	 */
-	private static function upgrade_products_1_5() {
-		global $wpdb;
-
-		$sql = "SELECT DISTINCT ID FROM {$wpdb->posts} as posts
-			JOIN {$wpdb->postmeta} as postmeta
-				ON posts.ID = postmeta.post_id
-				AND (postmeta.meta_key LIKE '_subscription%')
-			JOIN  {$wpdb->postmeta} AS soldindividually
-				ON posts.ID = soldindividually.post_id
-				AND ( soldindividually.meta_key LIKE '_sold_individually' AND soldindividually.meta_value !=  'yes' )
-			WHERE posts.post_type = 'product'";
-
-		$subscription_product_ids = $wpdb->get_results( $sql );
-
-		foreach ( $subscription_product_ids as $product_id ) {
-			update_post_meta( $product_id->ID, '_sold_individually', 'yes' );
-		}
-
-		// Update to new system to limit subscriptions by status rather than in a binary way
-		if ( '0' != self::$active_version && version_compare( self::$active_version, '1.5.4', '<' ) ) {
-			$wpdb->query(
-				"UPDATE $wpdb->postmeta
-				SET meta_value = 'any'
-				WHERE meta_key LIKE '_subscription_limit'
-				AND meta_value LIKE 'yes'"
-			);
-		}
-
-		return count( $subscription_product_ids );
-	}
-
-	/**
-	 * Update subscription WP-Cron tasks to Action Scheduler.
-	 *
-	 * @since 2.0
-	 */
-	private static function upgrade_hooks_1_5() {
-
-		$counter = 0;
-
-		$cron = _get_cron_array();
-
-		foreach ( $cron as $timestamp => $actions ) {
-			foreach ( $actions as $hook => $details ) {
-				if ( 'scheduled_subscription_payment' == $hook || 'scheduled_subscription_expiration' == $hook || 'scheduled_subscription_end_of_prepaid_term' == $hook || 'scheduled_subscription_trial_end' == $hook || 'paypal_check_subscription_payment' == $hook ) {
-					foreach ( $details as $hook_key => $values ) {
-
-						if ( ! wc_next_scheduled_action( $hook, $values['args'] ) ) {
-							wc_schedule_single_action( $timestamp, $hook, $values['args'] );
-							unset( $cron[ $timestamp ][ $hook ][ $hook_key ] );
-							$counter++;
-						}
-
-						if ( $counter >= self::$upgrade_limit_hooks ) {
-							break;
-						}
-					}
-
-					// If there are no other jobs scheduled for this hook at this timestamp, remove the entire hook
-					if ( 0 == count( $cron[ $timestamp ][ $hook ] ) ) {
-						unset( $cron[ $timestamp ][ $hook ] );
-					}
-					if ( $counter >= self::$upgrade_limit_hooks ) {
-						break;
-					}
-				}
-			}
-
-			// If there are no actions schedued for this timestamp, remove the entire schedule
-			if ( 0 == count( $cron[ $timestamp ] ) ) {
-				unset( $cron[ $timestamp ] );
-			}
-			if ( $counter >= self::$upgrade_limit_hooks ) {
-				break;
-			}
-		}
-
-		// Set the cron with the removed schedule
-		_set_cron_array( $cron );
-
-		return $counter;
 	}
 
 	/**
