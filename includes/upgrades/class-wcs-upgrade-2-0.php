031f145fc41ba935b2f4798117578702f3bc7ee0
thenbrent <thenbrent@gmail.com>
2015-06-04 22:16:42 -0700

    Initial commit of WCS_Upgrade_2_0
    The beginnings of #128

---
+++ includes/upgrades/class-wcs-upgrade-2-0.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Upgrade subscriptions data to v2.0
+ *
+ * @author		Prospress
+ * @category	Admin
+ * @package		WooCommerce Subscriptions/Admin/Upgrades
+ * @version		2.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+class WCS_Upgrade_2_0 {
+}
