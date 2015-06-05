8606f9d706c4fff18759f6ccb5877f210b7105b4
thenbrent <thenbrent@gmail.com>
2015-06-04 23:06:45 -0700

    Semantic tweaks to upgrade helper template
    Part of #128

--- includes/upgrades/templates/wcs-upgrade.php
+++ includes/upgrades/templates/wcs-upgrade.php
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<div id="update-welcome">
 			<h2><?php esc_html_e( 'Database Update Required', 'woocommerce-subscriptions' ); ?></h2>
 			<p><?php esc_html_e( 'The WooCommerce Subscriptions plugin has been updated!', 'woocommerce-subscriptions' ); ?></p>
-			<p><?php esc_html_e( 'Before we send you on your way, we need to update your database to the newest version. If you do not have a recent backup of your site, now is a good time to create one.', 'woocommerce-subscriptions' ); ?></p>
+			<p><?php esc_html_e( 'Before we send you on your way, we need to update your database to the newest version. If you do not have a recent backup of your site, now is the time to create one.', 'woocommerce-subscriptions' ); ?></p>
 			<p><?php esc_html_e( 'The update process may take a little while, so please be patient.', 'woocommerce-subscriptions' ); ?></p>
 			<form id="subscriptions-upgrade" method="get" action="<?php echo esc_url( admin_url( 'admin.php' ) ); ?>">
 				<input type="submit" class="button" value="<?php esc_attr_e( 'Update Database', 'woocommerce-subscriptions' ); ?>">
@@ -37,7 +37,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<div id="update-messages">
 			<h2><?php esc_html_e( 'Update in Progress', 'woocommerce-subscriptions' ); ?></h2>
 			<?php if ( 'false' == $script_data['really_old_version'] ) : ?>
-			<p><?php printf( esc_html__( 'The full update process for the %s subscriptions on your site will take approximately %s to %s minutes.', 'woocommerce-subscriptions' ), esc_html__( $subscription_count ), esc_html__( $estimated_duration ), esc_html__( $estimated_duration * 2 ) ); ?></p>
+			<p><?php printf( esc_html__( 'The full update process for the %s subscriptions on your site will take approximately %s to %s minutes.', 'woocommerce-subscriptions' ), esc_html( $subscription_count ), esc_html( $estimated_duration ), esc_html( $estimated_duration * 2 ) ); ?></p>
 			<?php endif; ?>
 			<p><?php esc_html_e( 'This page will display the results of the process as each batch of subscriptions is updated. No need to refresh or restart the process. Customers and other non-administrative users will continue to be able to browse your site without interuption while the update is in progress.', 'woocommerce-subscriptions' ); ?></p>
 			<ol>
@@ -46,7 +46,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		</div>
 		<div id="update-complete">
 			<h2><?php esc_html_e( 'Update Complete', 'woocommerce-subscriptions' ); ?></h2>
-			<p><?php esc_html_e( 'Your database has been successfully updated!', 'woocommerce-subscriptions' ); ?></p>
+			<p><?php esc_html_e( 'Your database has been updated successfully!', 'woocommerce-subscriptions' ); ?></p>
 			<p class="step"><a class="button" href="<?php echo esc_url( $about_page_url ); ?>"><?php esc_html_e( 'Continue', 'woocommerce-subscriptions' ); ?></a></p>
 			<p class="log-notice"><?php esc_html_e( sprintf( 'To record the progress of the upgrade a new log file was created. This file will be automatically deleted in %d weeks. If you would like to delete it sooner, you can find it here:', WCS_Upgrade_Logger::$weeks_until_cleanup, '<code>', wc_get_log_file_path( WCS_Upgrade_Logger::$handle ), '</code>' ), 'woocommerce-subscriptions' ); ?></small>
 			<code class="log-notice"><?php echo wc_get_log_file_path( WCS_Upgrade_Logger::$handle ); ?></code>
