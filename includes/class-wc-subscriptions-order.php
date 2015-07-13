3068e6e2505ac9c11e0553b9e076f004b65e4d75
matttallan <matt.d.a@live.com>
2015-07-13 11:17:31 +1000

    maybe_record_subscription_payment() handling the initial sub payment
    For #458

--- includes/class-wc-subscriptions-order.php
+++ includes/class-wc-subscriptions-order.php
@@ -39,9 +39,7 @@ class WC_Subscriptions_Order {
 		add_action( 'woocommerce_admin_order_data_after_order_details', __CLASS__ . '::contains_subscription_hidden_field', 10, 1 );
 
 		// Record initial payment against the subscription & set start date based on that payment
-		add_action( 'woocommerce_payment_complete', __CLASS__ . '::maybe_record_order_payment', 9, 1 );
-		add_action( 'woocommerce_order_status_processing', __CLASS__ . '::maybe_record_order_payment', 9, 1 );
-		add_action( 'woocommerce_order_status_completed', __CLASS__ . '::maybe_record_order_payment', 9, 1 );
+		add_action( 'woocommerce_payment_complete', __CLASS__ . '::maybe_record_subscription_payment', 9, 3 );
 
 		// Sometimes, even if the order total is $0, the order still needs payment
 		add_filter( 'woocommerce_order_needs_payment', __CLASS__ . '::order_needs_payment' , 10, 3 );
@@ -395,28 +393,36 @@ class WC_Subscriptions_Order {
 	/**
 	 * Records the initial payment against a subscription.
 	 *
-	 * This function is called when a gateway calls @see WC_Order::payment_complete() and payment
-	 * is completed on an order. It is also called when an orders status is changed to completed or
-	 * processing for those gateways which never call @see WC_Order::payment_complete(), like the
-	 * core WooCommerce Cheque and Bank Transfer gateways.
+	 * This function is called when an orders status is changed to completed or processing
+	 * for those gateways which never call @see WC_Order::payment_complete(), like the core
+	 * WooCommerce Cheque and Bank Transfer gateways.
 	 *
 	 * It will also set the start date on the subscription to the time the payment is completed.
 	 *
-	 * @param WC_Order|int $order A WC_Order object or ID of a WC_Order order.
-	 * @since 1.1.2
+	 * @param $order int|WC_Order
+	 * @param $old_order_status
+	 * @param $new_order_status
+	 * @since 2.0
 	 */
-	public static function maybe_record_order_payment( $order ) {
+	public static function maybe_record_subscription_payment( $order_id, $old_order_status, $new_order_status ) {
 
-		if ( ! wcs_order_contains_renewal( $order ) ) {
+		if ( ! wcs_order_contains_renewal( $order_id ) ) {
 
-			$subscriptions = wcs_get_subscriptions_for_order( $order );
+			$subscriptions = wcs_get_subscriptions_for_order( $order_id );
 
-			foreach ( $subscriptions as $subscription_id => $subscription ) {
+			foreach ( $subscriptions as $subscription ) {
 
-				// No payments have been recorded yet
-				if ( 0 == $subscription->get_completed_payment_count() ) {
-					$subscription->update_dates( array( 'start' => current_time( 'mysql', true ) ) );
-					$subscription->payment_complete();
+				// Do we need to activate a subscription?
+				if ( in_array( $new_order_status, array( 'processing', 'completed' ) ) && ! $subscription->has_status( 'active' ) ) {
+
+					if ( in_array( $old_order_status, apply_filters( 'woocommerce_valid_order_statuses_for_payment', array( 'pending', 'on-hold', 'failed' ) ) ) ) {
+						$subscription->update_dates( array( 'start' => current_time( 'mysql', true ) ) );
+						$subscription->payment_complete();
+					}
+
+				} elseif ( 'failed' == $old_order_status ) {
+
+					$subscription->payment_failed();
 				}
 			}
 		}
