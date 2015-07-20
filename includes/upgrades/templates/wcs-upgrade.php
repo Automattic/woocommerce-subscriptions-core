8821b22e4ba46532f035e4e6ddce2b76d4269eea
Gabor Javorszky <gabor@javorszky.co.uk>
2015-07-20 20:33:45 +0100

    i18n clean on includes/upgrades/templates/wcs-upgrade.php

--- includes/upgrades/templates/wcs-upgrade.php
+++ includes/upgrades/templates/wcs-upgrade.php
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 	<head>
-		<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php esc_attr_e( get_option( 'blog_charset' ) ); ?>" />
+		<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>"; charset="<?php echo esc_attr( get_option( 'blog_charset' ) ); ?>" />
 		<title><?php esc_html_e( 'WooCommerce Subscriptions Update', 'woocommerce-subscriptions' ); ?></title>
 		<?php wp_admin_css( 'install', true ); ?>
 		<?php wp_admin_css( 'ie', true ); ?>
@@ -28,15 +28,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<div id="update-welcome">
 			<h2><?php esc_html_e( 'Database Update Required', 'woocommerce-subscriptions' ); ?></h2>
 			<p><?php esc_html_e( 'The WooCommerce Subscriptions plugin has been updated!', 'woocommerce-subscriptions' ); ?></p>
-			<p><?php printf( esc_html__( 'Before we send you on your way, we need to update your database to the newest version. If you do not have a recent backup of your site, %snow is the time to create one%s.', 'woocommerce-subscriptions' ), '<a target="_blank" href="https://codex.wordpress.org/Backing_Up_Your_Database">', '</a>' ); ?></p>
+			<p><?php
+				/* translators: placeholders are opening and closing tags */
+				printf( esc_html__( 'Before we send you on your way, we need to update your database to the newest version. If you do not have a recent backup of your site, %snow is the time to create one%s.', 'woocommerce-subscriptions' ), '<a target="_blank" href="https://codex.wordpress.org/Backing_Up_Your_Database">', '</a>' ); ?>
+			</p>
 			<?php if ( 'false' == $script_data['really_old_version'] ) : ?>
-			<p><?php printf( esc_html__( 'The full update process for the %s subscriptions on your site will take between %d and %d minutes.', 'woocommerce-subscriptions' ), esc_html( $subscription_count ), round( $estimated_duration * 0.75 ), round( $estimated_duration * 1.5 ) ); ?></p>
+			<p><?php
+				/* translators: 1$: number of subscriptions on site, 2$, lower estimate (minutes), 3$: upper estimate */
+				printf( esc_html__( 'The full update process for the %1$s subscriptions on your site will take between %2$d and %3$d minutes.', 'woocommerce-subscriptions' ), esc_html( $subscription_count ), esc_html( round( $estimated_duration * 0.75 ) ), esc_html( round( $estimated_duration * 1.5 ) ) ); ?>
+			</p>
 			<?php else : ?>
 			<p><?php esc_html_e( 'The update process may take a little while, so please be patient.', 'woocommerce-subscriptions' ); ?></p>
 			<?php endif; ?>
 			<p><?php esc_html_e( 'Customers and other non-administrative users can browse and purchase from your store without interuption while the update is in progress.', 'woocommerce-subscriptions' ); ?></p>
 			<form id="subscriptions-upgrade" method="get" action="<?php echo esc_url( admin_url( 'admin.php' ) ); ?>">
-				<input type="submit" class="button" value="<?php esc_attr_e( 'Update Database', 'woocommerce-subscriptions' ); ?>">
+				<input type="submit" class="button" value="<?php echo esc_attr_x( 'Update Database', 'text on submit button', 'woocommerce-subscriptions' ); ?>">
 			</form>
 		</div>
 		<div id="update-messages">
@@ -58,8 +64,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<h2><?php esc_html_e( 'Update Complete', 'woocommerce-subscriptions' ); ?></h2>
 			<p><?php esc_html_e( 'Your database has been updated successfully!', 'woocommerce-subscriptions' ); ?></p>
 			<p class="step"><a class="button" href="<?php echo esc_url( $about_page_url ); ?>"><?php esc_html_e( 'Continue', 'woocommerce-subscriptions' ); ?></a></p>
-			<p class="log-notice"><?php esc_html_e( sprintf( 'To record the progress of the update a new log file was created. This file will be automatically deleted in %d weeks. If you would like to delete it sooner, you can find it here:', WCS_Upgrade_Logger::$weeks_until_cleanup, '<code>', wc_get_log_file_path( WCS_Upgrade_Logger::$handle ), '</code>' ), 'woocommerce-subscriptions' ); ?>
-			<code class="log-notice"><?php echo wc_get_log_file_path( WCS_Upgrade_Logger::$handle ); ?></code>
+			<p class="log-notice"><?php
+				/* translators: placeholder is number of weeks */
+				printf( esc_html__( 'To record the progress of the update a new log file was created. This file will be automatically deleted in %d weeks. If you would like to delete it sooner, you can find it here:', 'woocommerce-subscriptions' ), esc_html( WCS_Upgrade_Logger::$weeks_until_cleanup ) ); ?>
+			</p>
+			<code class="log-notice"><?php echo esc_html( wc_get_log_file_path( WCS_Upgrade_Logger::$handle ) ); ?></code>
 		</div>
 		<div id="update-error">
 			<h2><?php esc_html_e( 'Update Error', 'woocommerce-subscriptions' ); ?></h2>
