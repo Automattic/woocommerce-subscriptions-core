1e3d795a2e96f2df33d90daa7d6fc725d6d3e125
Max Rice <max@maxrice.com>
2015-08-21 19:37:25 -0400

    Fix placeholder issue with available gateways description

--- includes/admin/class-wc-subscriptions-admin.php
+++ includes/admin/class-wc-subscriptions-admin.php
@@ -905,8 +905,8 @@ class WC_Subscriptions_Admin {
 			// translators: placeholder is name of a gateway
 			$available_gateways_description = sprintf( __( 'The %s gateway can process automatic subscription payments.', 'woocommerce-subscriptions' ), '<strong>' . $available_gateways[0] . '</strong>' );
 		} elseif ( count( $available_gateways ) > 1 ) {
-			// translators: 1$: a comma separated list of gateway names (e.g. "stripe, paypal, worldpay"), 2$: one name of gateway (e.g. "authorize.net")
-			$available_gateways_description = sprintf( __( 'The %1$s & %s gateways can process automatic subscription payments.', 'woocommerce-subscriptions' ), '<strong>' . implode( '</strong>, <strong>', array_slice( $available_gateways, 0, count( $available_gateways ) - 1 ) ) . '</strong>', '<strong>' . array_pop( $available_gateways ) . '</strong>' );
+			// translators: %1$s - a comma separated list of gateway names (e.g. "stripe, paypal, worldpay"), %2$s - one name of gateway (e.g. "authorize.net")
+			$available_gateways_description = sprintf( __( 'The %1$s & %2$s gateways can process automatic subscription payments.', 'woocommerce-subscriptions' ), '<strong>' . implode( '</strong>, <strong>', array_slice( $available_gateways, 0, count( $available_gateways ) - 1 ) ) . '</strong>', '<strong>' . array_pop( $available_gateways ) . '</strong>' );
 		}
 
 		return apply_filters( 'woocommerce_subscription_settings', array(
