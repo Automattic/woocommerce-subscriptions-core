c84743f321edba99ccab3c1f29d1508eb8d64dc0
Brent Shepherd <thenbrent@gmail.com>
2015-09-21 15:02:07 -0700

    Fix args for can_subscription_be_changed_to hook
    Specifically, do not include a $subscription arg which was not included
    in v1.n

--- includes/deprecated/class-wcs-filter-deprecator.php
+++ includes/deprecated/class-wcs-filter-deprecator.php
@@ -324,7 +324,7 @@ class WCS_Filter_Deprecator extends WCS_Hook_Deprecator {
 				$args->order                      = self::get_order( $subscription );
 				$args->payment_gateway            = $subscription->payment_method;
 				$args->order_uses_manual_payments = $subscription->is_manual();
-				$return_value = apply_filters( $old_hook, $return_value, $subscription, $args );
+				$return_value = apply_filters( $old_hook, $return_value, $args );
 				break;
 		}
 
