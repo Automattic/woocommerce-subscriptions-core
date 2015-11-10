6ff97417839852d2a491c827b6c64ea64353c793
matttallan <matt.d.a@live.com>
2015-11-10 16:40:06 +1000

    logger function to always log uncaught exceptions in ipn handler (subs)
    There's a few pieces to this function.
    Firstly, it will log the last fatal error/uncaught exception that was
    thrown when handling a subscription IPN request.
    Logging these uncaught fatal errors will be logged under
    wcs-paypal-SHA.log

--- includes/gateways/paypal/includes/class-wcs-paypal-standard-ipn-failure-handler.php
+++ includes/gateways/paypal/includes/class-wcs-paypal-standard-ipn-failure-handler.php
@@ -29,6 +29,34 @@ class WCS_PayPal_Standard_IPN_Failure_Handler {
 	public static function init() {
 
 	}
+
+	/**
+	 * Log any fatal errors occurred while Subscriptions is trying to process IPN messages
+	 *
+	 * @since 2.0.6
+	 * @param array $transaction_details the current IPN message being processed when the fatal error occurred
+	 * @param array $error
+	 */
+	public static function log_ipn_errors( $transaction_details, $error = '' ) {
+		// we want to make sure the ipn error admin notice is always displayed when a new error occurs
+		delete_option( 'wcs_fatal_error_handling_ipn_ignored' );
+
+		if ( empty( self::$log ) ) {
+			self::$log = new WC_Logger();
+		}
+
+		self::$log->add( 'wcs-paypal', sprintf( 'Subscription transaction details: %s', print_r( $transaction_details, true ) ) );
+		$message = get_option( 'wcs_fatal_error_handling_ipn', '' );
+
+		if ( ! empty( $message ) ) {
+			self::$log->add( 'wcs-paypal', sprintf( 'Subscription exception caught: %s', $message ) );
+			WC_Gateway_PayPal::log( $message );
+		}
+
+		if ( ! empty( $error ) ) {
+			self::$log->add( 'wcs-paypal', sprintf( __( 'Unexcepted shutdown when processing subscription IPN messages. PHP Fatal error %s in %s on line %s.', 'woocommerce-subscriptions' ), $error['message'], $error['file'], $error['line'] ) );
+		}
+	}
 }
 
 WCS_PayPal_Standard_IPN_Failure_Handler::init();
