40a5ec83e9a8c34a88e531a0a0b1bd4c24025469
Kathy Darling <helgatheviking@gmail.com>
2016-01-06 08:42:05 -0500

    use existing function wcs_get_canonical_product_id(). thanks @javorszky

--- includes/class-wc-subscriptions-switcher.php
+++ includes/class-wc-subscriptions-switcher.php
@@ -456,7 +456,7 @@ class WC_Subscriptions_Switcher {
 	 */
 	public static function can_item_be_switched( $item, $subscription = null ) {
 
-		$product_id = isset( $item['variation_id'] ) ? $item['variation_id'] : $item['product_id'];
+		$product_id = wcs_get_canonical_product_id( $item );
 
 		if ( 'line_item' == $item['type'] && wcs_is_product_switchable_type( $product_id ) ) {
 			$is_product_switchable = true;
