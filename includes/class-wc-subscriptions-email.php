c546a56b97a2b60f1629f130da2688f0a5f2e12c
Gabor Javorszky <gabor@javorszky.co.uk>
2016-05-09 09:09:39 +0100

    Add suspended email to the list

--- includes/class-wc-subscriptions-email.php
+++ includes/class-wc-subscriptions-email.php
@@ -44,15 +44,17 @@ class WC_Subscriptions_Email {
 		require_once( 'emails/class-wcs-email-customer-renewal-invoice.php' );
 		require_once( 'emails/class-wcs-email-cancelled-subscription.php' );
 		require_once( 'emails/class-wcs-email-expired-subscription.php' );
+		require_once( 'emails/class-wcs-email-suspended-subscription.php' );
 
-		$email_classes['WCS_Email_New_Renewal_Order']        = new WCS_Email_New_Renewal_Order();
-		$email_classes['WCS_Email_New_Switch_Order']         = new WCS_Email_New_Switch_Order();
-		$email_classes['WCS_Email_Processing_Renewal_Order'] = new WCS_Email_Processing_Renewal_Order();
-		$email_classes['WCS_Email_Completed_Renewal_Order']  = new WCS_Email_Completed_Renewal_Order();
-		$email_classes['WCS_Email_Completed_Switch_Order']   = new WCS_Email_Completed_Switch_Order();
-		$email_classes['WCS_Email_Customer_Renewal_Invoice'] = new WCS_Email_Customer_Renewal_Invoice();
-		$email_classes['WCS_Email_Cancelled_Subscription']   = new WCS_Email_Cancelled_Subscription();
-		$email_classes['WCS_Email_Expired_Subscription']     = new WCS_Email_Expired_Subscription();
+		$email_classes['WCS_Email_New_Renewal_Order']          = new WCS_Email_New_Renewal_Order();
+		$email_classes['WCS_Email_New_Switch_Order']           = new WCS_Email_New_Switch_Order();
+		$email_classes['WCS_Email_Processing_Renewal_Order']   = new WCS_Email_Processing_Renewal_Order();
+		$email_classes['WCS_Email_Completed_Renewal_Order']    = new WCS_Email_Completed_Renewal_Order();
+		$email_classes['WCS_Email_Completed_Switch_Order']     = new WCS_Email_Completed_Switch_Order();
+		$email_classes['WCS_Email_Customer_Renewal_Invoice']   = new WCS_Email_Customer_Renewal_Invoice();
+		$email_classes['WCS_Email_Cancelled_Subscription']     = new WCS_Email_Cancelled_Subscription();
+		$email_classes['WCS_Email_Expired_Subscription']       = new WCS_Email_Expired_Subscription();
+		$email_classes['WCS_Email_Suspended_Subscription']     = new WCS_Email_Suspended_Subscription();
 
 		return $email_classes;
 	}
@@ -71,6 +73,7 @@ class WC_Subscriptions_Email {
 
 		add_action( 'woocommerce_subscription_status_updated', __CLASS__ . '::send_cancelled_email', 10, 2 );
 		add_action( 'woocommerce_subscription_status_expired', __CLASS__ . '::send_expired_email', 10, 2 );
+		add_action( 'woocommerce_customer_changed_subscription_to_on-hold', __CLASS__ . '::send_suspended_email', 10, 2 );
 
 		$order_email_actions = array(
 			'woocommerce_order_status_pending_to_processing',
@@ -117,6 +120,20 @@ class WC_Subscriptions_Email {
 
 		if ( 'true' !== get_post_meta( $subscription->id, '_expired_email_sent', true ) ) {
 			do_action( 'expired_subscription_notification', $subscription );
+		}
+	}
+
+	/**
+	 * Init the mailer and call for the suspended email notification hook.
+	 *
+	 * @param $subscription WC Subscription
+	 * @since 2.1
+	 */
+	public static function send_suspended_email( $subscription ) {
+		WC()->mailer();
+
+		if ( 'true' !== get_post_meta( $subscription->id, '_suspended_email_sent', true ) ) {
+			do_action( 'suspended_subscription_notification', $subscription );
 		}
 	}
 
