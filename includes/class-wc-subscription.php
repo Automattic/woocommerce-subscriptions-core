ff08419166c36a2b11aa76ac1334c33572ac4154
matttallan <matt.d.a@live.com>
2016-05-16 17:25:32 +1000

    Check number of completed payments is greater than 1 for renewal fails
    Fixes #1389
     - this condition is fine when calling
       woocommerce_subscription_renewal_payment_complete because at this
    payment there should be at least 2 orders that have completed (the
    original and the first renewal) but the same condition copied to
    handling the renewal payment failed hook breaks.

--- includes/class-wc-subscription.php
+++ includes/class-wc-subscription.php
@@ -1304,7 +1304,7 @@ class WC_Subscription extends WC_Order {
 
 		do_action( 'woocommerce_subscription_payment_failed', $this, $new_status );
 
-		if ( $this->get_completed_payment_count() > 1 ) {
+		if ( $this->get_completed_payment_count() >= 1 ) {
 			do_action( 'woocommerce_subscription_renewal_payment_failed', $this );
 		}
 	}
