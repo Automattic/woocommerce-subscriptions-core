77f14db798c8bde768959335b4f1b7b89e960a5d
matttallan <matt.d.a@live.com>
2016-06-01 18:05:28 +1000

    Add subscription meta to get_item response for subscription post types
     - most of the work is done by extending the WC_REST_Orders_Controller
       class

--- includes/api/class-wc-rest-subscriptions-controller.php
+++ includes/api/class-wc-rest-subscriptions-controller.php
@@ -34,4 +34,37 @@ class WC_REST_Subscriptions_Controller extends WC_REST_Orders_Controller {
 	 */
 	protected $post_type = 'shop_subscription';
 
+	/**
+	 * Initialize orders actions and filters
+	 */
+	public function __construct() {
+		add_filter( 'woocommerce_rest_prepare_shop_subscription', array( $this, 'filter_get_subscription_response' ), 10, 3 );
+	}
+
+	/**
+	 * Filter WC_REST_Orders_Controller::get_item response for subscription post types
+	 *
+	 * @since 2.1
+	 * @param WP_REST_Response $response
+	 * @param WP_POST $post
+	 */
+	public function filter_get_subscription_response( $response, $post, $request ) {
+
+		if ( ! empty( $post->post_type ) && ! empty( $post->ID ) && 'shop_subscription' == $post->post_type ) {
+			$subscription = wcs_get_subscription( $post->ID );
+
+			$response->data['billing_period']   = $subscription->billing_period;
+			$response->data['billing_interval'] = $subscription->billing_interval;
+
+			$response->data['schedule'] = array (
+				'start_date'        => wc_rest_prepare_date_response( $subscription->get_date( 'start' ) ),
+				'trial_end_date'    => wc_rest_prepare_date_response( $subscription->get_date( 'trial_end' ) ),
+				'next_payment_date' => wc_rest_prepare_date_response( $subscription->get_date( 'next_payment' ) ),
+				'end_date'          => wc_rest_prepare_date_response( $subscription->get_date( 'end' ) ),
+			);
+		}
+
+		return $response;
+	}
+
 }
