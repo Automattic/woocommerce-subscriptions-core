d896e1d60126f3f71e9093018a08feb37aa2bc31
matttallan <matt.d.a@live.com>
2016-06-02 11:53:53 +1000

    First parse at the `/subscriptions/<id>/orders/` endpoint
    - gets all related subscription orders
    - at the moment, the endpoint will just return them all and not page
      them

--- includes/api/class-wc-rest-subscriptions-controller.php
+++ includes/api/class-wc-rest-subscriptions-controller.php
@@ -42,6 +42,23 @@ class WC_REST_Subscriptions_Controller extends WC_REST_Orders_Controller {
 	}
 
 	/**
+	 * Register the routes for orders.
+	 */
+	public function register_routes() {
+		parent::register_routes();
+
+		register_rest_route( $this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)/orders', array(
+			array(
+				'methods'             => WP_REST_Server::READABLE,
+				'callback'            => array( $this, 'get_subscription_orders' ),
+				'permission_callback' => array( $this, 'get_items_permissions_check' ),
+				'args'                => $this->get_collection_params(),
+			),
+			'schema' => array( $this, 'get_public_item_schema' ),
+		) );
+	}
+
+	/**
 	 * Filter WC_REST_Orders_Controller::get_item response for subscription post types
 	 *
 	 * @since 2.1
@@ -67,4 +84,40 @@ class WC_REST_Subscriptions_Controller extends WC_REST_Orders_Controller {
 		return $response;
 	}
 
+	/**
+	 * Get subscription orders
+	 *
+	 * @since 2.1
+	 * @param WP_REST_Request $request
+	 * @return WP_Error|WP_REST_Response $response
+	 */
+	public function get_subscription_orders( $request ) {
+		$id  = (int) $request['id'];
+
+		if ( empty( $id ) || ! wcs_is_subscription( $id ) ) {
+			return new WP_Error( "woocommerce_rest_invalid_shop_subscription_id", __( 'Invalid subscription id.', 'woocommerce-subscriptions' ), array( 'status' => 404 ) );
+		}
+
+		$this->post_type     = 'shop_order';
+		$subscription        = wcs_get_subscription( $id );
+		$subscription_orders = $subscription->get_related_orders();
+
+		$orders = array();
+
+		foreach ( $subscription_orders as $order_id ) {
+			$post = get_post( $order_id );
+			if ( ! wc_rest_check_post_permissions( $this->post_type, 'read', $post->ID ) ) {
+				continue;
+			}
+
+			$data     = $this->prepare_item_for_response( $post, $request );
+			$orders[] = $this->prepare_response_for_collection( $data );
+		}
+
+		$response = rest_ensure_response( $orders );
+		$response->header( 'X-WP-Total', count( $orders ) );
+		$response->header( 'X-WP-TotalPages', 1 );
+
+		return apply_filters( 'wcs_rest_subscription_orders_response', $response, $request );
+	}
 }
