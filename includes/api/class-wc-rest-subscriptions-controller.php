feb431ae3a4dc245ea3fe0028be90e62183f1f1c
matttallan <matt.d.a@live.com>
2016-06-24 13:10:56 +1000

    Move dates out of the scheduled array
     - it was unecessary to store send subscription dates in the response
       inside $data['scheduled'] and caused confusion in the docs when
    writing out the subscription properties.. i.e. data['start_date'] it
    editable field and data['schedule']['start_date'] is a read only field.

--- includes/api/class-wc-rest-subscriptions-controller.php
+++ includes/api/class-wc-rest-subscriptions-controller.php
@@ -82,15 +82,12 @@ class WC_REST_Subscriptions_Controller extends WC_REST_Orders_Controller {
 		if ( ! empty( $post->post_type ) && ! empty( $post->ID ) && 'shop_subscription' == $post->post_type ) {
 			$subscription = wcs_get_subscription( $post->ID );
 
-			$response->data['billing_period']   = $subscription->billing_period;
-			$response->data['billing_interval'] = $subscription->billing_interval;
-
-			$response->data['schedule'] = array(
-				'start_date'        => wc_rest_prepare_date_response( $subscription->get_date( 'start' ) ),
-				'trial_end_date'    => wc_rest_prepare_date_response( $subscription->get_date( 'trial_end' ) ),
-				'next_payment_date' => wc_rest_prepare_date_response( $subscription->get_date( 'next_payment' ) ),
-				'end_date'          => wc_rest_prepare_date_response( $subscription->get_date( 'end' ) ),
-			);
+			$response->data['billing_period']    = $subscription->billing_period;
+			$response->data['billing_interval']  = $subscription->billing_interval;
+			$response->data['start_date']        = wc_rest_prepare_date_response( $subscription->get_date( 'start' ) );
+			$response->data['trial_end_date']    = wc_rest_prepare_date_response( $subscription->get_date( 'trial_end' ) );
+			$response->data['next_payment_date'] = wc_rest_prepare_date_response( $subscription->get_date( 'next_payment' ) );
+			$response->data['end_date']          = wc_rest_prepare_date_response( $subscription->get_date( 'end_date' ) );
 		}
 
 		return $response;
