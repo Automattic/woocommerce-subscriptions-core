6700eac204ec42122245f52a2ecc4861b5555871
Brent Shepherd <thenbrent@gmail.com>
2016-07-14 15:38:31 -0700

    Add new wcs_admin_display_date_type filter
    To allow dates to be shown/hidden based on subscription details. Of
    particular use for the retry payment date and the custom shipping
    schedule dates, both of which only need to be displayed when there is
    actually a date.

--- includes/admin/meta-boxes/views/html-subscription-schedule.php
+++ includes/admin/meta-boxes/views/html-subscription-schedule.php
@@ -44,7 +44,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</div>
 
 	<?php foreach ( wcs_get_subscription_date_types() as $date_key => $date_label ) : ?>
-		<?php if ( 'last_payment' === $date_key ) : ?>
+		<?php $display_date_type = ( 'last_payment' === $date_key ) ? false : true; ?>
+		<?php if ( false === apply_filters( 'wcs_admin_display_date_type', $display_date_type, $date_key, $the_subscription ) ) : ?>
 			<?php continue; ?>
 		<?php endif;?>
 	<div id="subscription-<?php echo esc_attr( $date_key ); ?>-date" class="date-fields">
