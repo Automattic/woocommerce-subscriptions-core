e901cfe177834bd1f44c1126200d39b66c79e6a6
Brent Shepherd <thenbrent@gmail.com>
2016-11-17 15:12:38 -0800

    Replace references to WooThemes

--- includes/emails/class-wcs-email-customer-completed-switch-order.php
+++ includes/emails/class-wcs-email-customer-completed-switch-order.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @class 		WCS_Email_Completed_Switch_Order
  * @version		2.0.0
  * @package		WooCommerce/Classes/Emails
- * @author 		WooThemes
+ * @author 		Prospress
  * @extends 	WC_Email
  */
 class WCS_Email_Completed_Switch_Order extends WC_Email_Customer_Completed_Order {
