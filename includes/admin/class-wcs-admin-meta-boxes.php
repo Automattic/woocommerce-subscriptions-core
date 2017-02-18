9a0f0c20b6a94d8f82c76e04cb2dda1894af88fe
Brent Shepherd <brent@prospress.com>
2017-02-17 20:37:34 -0800

    Set $supports_date_changes default
    To avoid undefined variable warnings

--- includes/admin/class-wcs-admin-meta-boxes.php
+++ includes/admin/class-wcs-admin-meta-boxes.php
@@ -230,6 +230,7 @@ class WCS_Admin_Meta_Boxes {
 		$can_be_retried = false;
 
 		if ( wcs_order_contains_renewal( $order ) && $order->needs_payment() && ! empty( $order->payment_method ) ) {
+			$supports_date_changes          = false;
 			$order_payment_gateway          = wc_get_payment_gateway_by_order( $order );
 			$order_payment_gateway_supports = ( isset( $order_payment_gateway->id ) ) ? has_action( 'woocommerce_scheduled_subscription_payment_' . $order_payment_gateway->id ) : false;
 
