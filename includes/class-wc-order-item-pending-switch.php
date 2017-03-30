fb8f604b7feb65df965ae93ca14b7481b43b27c3
Cesar Rodas <crodas@php.net>
2017-03-30 09:25:36 -0400

    Moved data store registration to its right place

--- includes/class-wc-order-item-pending-switch.php
+++ includes/class-wc-order-item-pending-switch.php
@@ -4,11 +4,4 @@ class WC_Order_Item_Pending_Switch extends WC_Order_Item_Product {
 	public function get_type() {
 		return 'line_item_pending_switch';
 	}
-
-	public static function init( $stores ) {
-		$stores['order-item-line_item_pending_switch'] = 'WC_Order_Item_Product_Data_Store';
-		return $stores;
-	}
 }
-
-add_filter( 'woocommerce_data_stores', 'WC_Order_Item_Pending_Switch::init' );
