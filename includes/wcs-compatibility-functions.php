7c0833bca5fd18a151b7d32c852a88ea8d37deb0
LCmry <lcrees.2014@gmail.com>
2017-04-14 12:50:42 -0700

    Get object meta by key To prevent creating extra instances of the same meta data, and thus possibly retrieving the wrong meta values. This function gets the id for the meta data by the key or returns the preexisting meta id.

--- includes/wcs-compatibility-functions.php
+++ includes/wcs-compatibility-functions.php
@@ -265,6 +265,7 @@ function wcs_set_objects_property( &$object, $key, $value, $save = 'save', $meta
 
 	// If there is no setter, treat as meta within the 3.0.x object.
 	} elseif ( is_callable( array( $object, 'update_meta_data' ) ) ) {
+		$meta_id = wcs_maybe_get_meta_id_by_key( $object, $prefixed_key, $meta_id );
 		$object->update_meta_data( $prefixed_key, $value, $meta_id );
 
 	// 2.6.x handling for name which is not meta.
@@ -293,6 +294,36 @@ function wcs_set_objects_property( &$object, $key, $value, $save = 'save', $meta
 			}
 		}
 	}
+}
+
+/**
+* Gets an object's meta object's id based on key.
+*
+* @param WC_Order|WC_Product|WC_Subscription $object The object whose meta data we want to access.
+* @param string $key The meta key, without '_' prefix, whose id we want to get
+* @param int $meta_id The meta ID of existing meta data
+* @return mixed meta id if object's meta exists and can be accessed
+* @since 2.2.5
+*/
+function wcs_maybe_get_meta_id_by_key( &$object, $key, $meta_id = '' ) {
+	
+	$object_meta = array();
+
+	$prefixed_key = wcs_maybe_prefix_key( $key );
+
+	if ( empty( $meta_id ) ) {
+	
+		if ( is_callable( array($object, 'get_meta_data' ) ) ) {
+			$object_meta = $object->get_meta_data();
+		}
+
+		if ( ! empty( $object_meta ) ) {
+			$array_keys = array_keys( wp_list_pluck( $object_meta, 'key' ), $prefixed_key );
+			$meta_id = property_exists( $object_meta[ current( $array_keys ) ], 'id' ) ? $object_meta[ current( $array_keys ) ]->id : '';
+		}
+	}
+
+	return $meta_id;
 }
 
 /**
