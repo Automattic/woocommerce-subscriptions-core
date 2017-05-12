171c23275f671b781128bf3a894e79acf650459f
LCmry <lcrees.2014@gmail.com>
2017-05-12 16:44:49 -0700

    Check that 'get_current_screen' can be called

--- includes/admin/class-wc-subscriptions-admin.php
+++ includes/admin/class-wc-subscriptions-admin.php
@@ -1525,19 +1525,22 @@ class WC_Subscriptions_Admin {
 	*/
 	public static function change_order_item_editable_text( $translated_text, $text, $domain ) {
 
-		$screen = get_current_screen();
+		if ( is_admin() && function_exists( 'get_current_screen' ) ) {
 
-		if ( is_admin() && 'shop_subscription' == $screen->id ) {
+			$screen = get_current_screen();
 
-			switch ( $text ) {
+			if ( 'shop_subscription' == $screen->id ) {
 
-				case 'This order is no longer editable.':
-					$translated_text = __( 'Subscription items can no longer be edited.', 'woocommerce-subscriptions' );
-					break;
+				switch ( $text ) {
 
-				case 'To edit this order change the status back to "Pending"':
-					$translated_text = __( 'This subscription is no longer editable because the payment gateway does not allow modification of recurring amounts.', 'woocommerce-subscriptions' );
-					break;
+					case 'This order is no longer editable.':
+						$translated_text = __( 'Subscription items can no longer be edited.', 'woocommerce-subscriptions' );
+						break;
+
+					case 'To edit this order change the status back to "Pending"':
+						$translated_text = __( 'This subscription is no longer editable because the payment gateway does not allow modification of recurring amounts.', 'woocommerce-subscriptions' );
+						break;
+				}
 			}
 		}
 		return $translated_text;
