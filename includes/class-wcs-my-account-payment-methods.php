dcb9125a47e3cdf00d276c98b6ca2f4fe18837d8
James Allan <james.allan@live.com>
2017-05-25 15:56:21 +1000

    Display notice after updating subscription payment meta
    After deleting a token related to automatic subscription payments,
    display a notice to the customer.

--- includes/class-wcs-my-account-payment-methods.php
+++ includes/class-wcs-my-account-payment-methods.php
@@ -66,49 +66,54 @@ class WCS_My_Account_Payment_Methods {
 	 * @since 2.2.7
 	 */
 	public static function maybe_update_subscriptions_payment_meta( $deleted_token_id, $deleted_token ) {
-
-		if ( isset( $_GET['delete_and_update_subscription_token'] ) ) {
-
+		if ( isset( $_GET['delete_subscription_token'] ) && ! empty( $_GET['wcs_nonce'] ) && wp_verify_nonce( $_GET['wcs_nonce'], 'delete_subscription_token_' . $_GET['delete_subscription_token'] ) ) {
 			// init payment gateways
 			WC()->payment_gateways();
 
-			$new_token = WC_Payment_Tokens::get( $_GET['delete_and_update_subscription_token'] );
+			$new_token = self::get_customers_alternative_token( $deleted_token );
 
 			if ( empty( $new_token ) ) {
+				$notice = esc_html__( 'The deleted payment method was used for automatic subscription payments, we couldn\'t find an alternative token payment method token to change your subscriptions to.', 'woocommerce-subscriptions' );
+				wc_add_notice( $notice, 'error' );
 				return;
 			}
 
-			$subscriptions  = self::get_subscriptions_by_token( $deleted_token );
-			$token_meta_key = '';
+			$subscriptions    = self::get_subscriptions_by_token( $deleted_token );
+			$token_meta_key   = '';
+			$notice_displayed = false;
 
-			foreach ( $subscriptions as $subscription ) {
-				$subscription = wcs_get_subscription( $subscription );
+			if ( ! empty( $subscriptions ) ) {
+				wc_add_notice( sprintf( esc_html__( 'The deleted payment method was used for automatic subscription payments. To avoid failed renewal payments in future the subscriptions using this payment method have been updated to use %s.', 'woocommerce-subscriptions' ), self::get_token_label( $new_token ) ), 'notice' );
 
-				if ( empty( $subscription ) ) {
-					continue;
-				}
+				foreach ( $subscriptions as $subscription ) {
+					$subscription = wcs_get_subscription( $subscription );
 
-				// Attempt to find the token meta key if we haven't already found it.
-				if ( empty( $token_meta_key ) ) {
-					$payment_method_meta = apply_filters( 'woocommerce_subscription_payment_meta', array(), $subscription );
+					if ( empty( $subscription ) ) {
+						continue;
+					}
 
-					if ( is_array( $payment_method_meta ) && isset( $payment_method_meta[ $deleted_token->get_gateway_id() ] ) && is_array( $payment_method_meta[ $deleted_token->get_gateway_id() ] ) ) {
-						foreach ( $payment_method_meta[ $deleted_token->get_gateway_id() ] as $meta_table => $meta ) {
-							foreach ( $meta as $meta_key => $meta_data ) {
-								if ( $deleted_token->get_token() === $meta_data['value'] ) {
-									$token_meta_key = $meta_key;
-									break 2;
+					// Attempt to find the token meta key if we haven't already found it.
+					if ( empty( $token_meta_key ) ) {
+						$payment_method_meta = apply_filters( 'woocommerce_subscription_payment_meta', array(), $subscription );
+
+						if ( is_array( $payment_method_meta ) && isset( $payment_method_meta[ $deleted_token->get_gateway_id() ] ) && is_array( $payment_method_meta[ $deleted_token->get_gateway_id() ] ) ) {
+							foreach ( $payment_method_meta[ $deleted_token->get_gateway_id() ] as $meta_table => $meta ) {
+								foreach ( $meta as $meta_key => $meta_data ) {
+									if ( $deleted_token->get_token() === $meta_data['value'] ) {
+										$token_meta_key = $meta_key;
+										break 2;
+									}
 								}
 							}
 						}
 					}
-				}
 
-				$updated = update_post_meta( $subscription->get_id(), $token_meta_key, $new_token->get_token(), $deleted_token->get_token() );
+					$updated = update_post_meta( $subscription->get_id(), $token_meta_key, $new_token->get_token(), $deleted_token->get_token() );
 
-				if ( $updated ) {
-					$subscription->add_order_note( sprintf( _x( 'Payment method meta updated after customer deleted a token from their My Account page. Payment meta changed from %1$s to %2$s', 'used in subscription note', 'woocommerce-subscriptions' ), $deleted_token->get_token(), $new_token->get_token() ) );
-					do_action( 'woocommerce_subscription_token_changed', $subscription, $new_token, $deleted_token );
+					if ( $updated ) {
+						$subscription->add_order_note( sprintf( _x( 'Payment method meta updated after customer deleted a token from their My Account page. Payment meta changed from %1$s to %2$s', 'used in subscription note', 'woocommerce-subscriptions' ), $deleted_token->get_token(), $new_token->get_token() ) );
+						do_action( 'woocommerce_subscription_token_changed', $subscription, $new_token, $deleted_token );
+					}
 				}
 			}
 		}
