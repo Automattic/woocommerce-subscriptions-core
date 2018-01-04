e51bd911e303f430755ffe747334b3e34794ddda
James Allan <james.allan@live.com>
2018-01-04 17:15:13 +1000

    Separate render_system_status_items into smaller more distinct functions

--- includes/admin/class-wcs-admin-system-status.php
+++ includes/admin/class-wcs-admin-system-status.php
@@ -12,13 +12,15 @@
  */
 class WCS_Admin_System_Status {
 
+	const WCS_PRODUCT_ID = 27147;
+
 	/**
 	 * Attach callbacks
 	 *
 	 * @since 2.3.0
 	 */
 	public static function init() {
-		add_filter( 'woocommerce_system_status_report', __CLASS__ . '::render_system_status_items' );
+		add_filter( 'woocommerce_system_status_report', array( __CLASS__, 'render_system_status_items' ) );
 	}
 
 	/**
@@ -27,65 +29,17 @@ class WCS_Admin_System_Status {
 	 * @since 2.3.0
 	 */
 	public static function render_system_status_items() {
-		$debug_data   = array();
-		$is_wcs_debug = defined( 'WCS_DEBUG' ) ? WCS_DEBUG : false;
+		$debug_data = array();
 
-		$debug_data['wcs_debug'] = array(
-			'name'    => _x( 'WCS_DEBUG', 'label that indicates whether debugging is turned on for the plugin', 'woocommerce-subscriptions' ),
-			'note'    => ( $is_wcs_debug ) ? __( 'Yes', 'woocommerce-subscriptions' ) :  __( 'No', 'woocommerce-subscriptions' ),
-			'success' => $is_wcs_debug ? 0 : 1,
-		);
+		self::set_debug_mode( $debug_data );
 
-		$debug_data['wcs_staging'] = array(
-			'name'    => _x( 'Subscriptions Mode', 'Live or Staging, Label on WooCommerce -> System Status page', 'woocommerce-subscriptions' ),
-			'note'    => '<strong>' . ( ( WC_Subscriptions::is_duplicate_site() ) ? _x( 'Staging', 'refers to staging site', 'woocommerce-subscriptions' ) :  _x( 'Live', 'refers to live site', 'woocommerce-subscriptions' ) ) . '</strong>',
-			'success' => ( WC_Subscriptions::is_duplicate_site() ) ? 0 : 1,
-		);
+		self::set_staging_mode( $debug_data );
 
-		$theme_overrides = self::get_theme_overrides();
-		$debug_data['wcs_theme_overrides'] = array(
-			'name'      => _x( 'Subscriptions Template Theme Overrides', 'label for the system status page', 'woocommerce-subscriptions' ),
-			'data'      => $theme_overrides,
-		);
+		self::set_theme_overrides( $debug_data );
 
-		$debug_data['wcs_subscriptions_by_status'] = array(
-			'name'      => _x( 'Subscription Statuses', 'label for the system status page', 'woocommerce-subscriptions' ),
-			'mark'      => '',
-			'mark_icon' => '',
-			'data'      => self::get_subscriptions_statuses(),
-		);
+		self::set_subscription_statuses( $debug_data );
 
-		// Check for a connected WooCommerce account and active Subscriptions product key
-		if ( class_exists( 'WC_Helper' ) ) {
-			$woocommerce_account_auth      = WC_Helper_Options::get( 'auth' );
-			$woocommerce_account_connected = ! empty( $woocommerce_account_auth );
-
-			$debug_data['wcs_woocommerce_account_connected'] = array(
-				'name'      => _x( 'WooCommerce Account Connected', 'label for the system status page', 'woocommerce-subscriptions' ),
-				'mark_icon' => $woocommerce_account_connected ? 'yes' : 'warning',
-				'note'      => $woocommerce_account_connected ? 'Yes' : 'No',
-				'success'   => $woocommerce_account_connected,
-			);
-
-			if ( $woocommerce_account_connected ) {
-				$woocommerce_account_subscriptions = WC_Helper::get_subscriptions();
-				$site_id                           = absint( $woocommerce_account_auth['site_id'] );
-
-				foreach ( $woocommerce_account_subscriptions as $subscription ) {
-					if ( isset( $subscription['product_id'] ) && 27147 === $subscription['product_id'] ) {
-						$active = in_array( $site_id, $subscription['connections'] );
-
-						$debug_data['wcs_active_product_key'] = array(
-							'name'      => _x( 'Active Product Key', 'label for the system status page', 'woocommerce-subscriptions' ),
-							'mark_icon' => $active ? 'yes' : 'no',
-							'note'      => $active ? 'Yes' : 'No',
-							'success'   => $active,
-						);
-						break;
-					}
-				}
-			}
-		}
+		self::set_woocommerce_account_data( $debug_data );
 
 		$debug_data      = apply_filters( 'wcs_system_status', $debug_data );
 		$section_title   = __( 'Subscriptions', 'woocommerce-subscriptions' );
@@ -93,12 +47,61 @@ class WCS_Admin_System_Status {
 
 		include( plugin_dir_path( WC_Subscriptions::$plugin_file ) . 'templates/admin/status.php' );
 
-		// Payment Gateways
-		$debug_data      = self::get_subscriptions_payment_gateway_data();
-		$section_title   = __( 'Payment Gateways', 'woocommerce-subscriptions' );
-		$section_tooltip = __( 'This section shows information about Payment methods.', 'woocommerce-subscriptions' );
+		// Subscriptions by Payment Gateway
+		$debug_data = array();
+
+		self::set_subscriptions_by_payment_gateway( $debug_data );
+
+		$section_title   = __( 'Subscriptions by Payment Gateway', 'woocommerce-subscriptions' );
+		$section_tooltip = __( 'This section shows information about Subscription payment methods.', 'woocommerce-subscriptions' );
 
 		include( plugin_dir_path( WC_Subscriptions::$plugin_file ) . 'templates/admin/status.php' );
+
+		// Payment gateway features
+		$debug_data = array();
+
+		self::set_subscriptions_payment_gateway_support( $debug_data );
+
+		$section_title   = __( 'Payment Gateway Support', 'woocommerce-subscriptions' );
+		$section_tooltip = __( 'This section shows information about payment gateway feature support.', 'woocommerce-subscriptions' );
+
+		include( plugin_dir_path( WC_Subscriptions::$plugin_file ) . 'templates/admin/status.php' );
+	}
+
+	/**
+	 * Include WCS_DEBUG flag
+	 */
+	private static function set_debug_mode( &$debug_data ) {
+		$is_wcs_debug = defined( 'WCS_DEBUG' ) ? WCS_DEBUG : false;
+
+		$debug_data['wcs_debug'] = array(
+			'name'    => _x( 'WCS_DEBUG', 'label that indicates whether debugging is turned on for the plugin', 'woocommerce-subscriptions' ),
+			'note'    => ( $is_wcs_debug ) ? __( 'Yes', 'woocommerce-subscriptions' ) :  __( 'No', 'woocommerce-subscriptions' ),
+			'success' => $is_wcs_debug ? 0 : 1,
+		);
+	}
+
+	/**
+	 * Include the staging/live mode the store is running in.
+	 */
+	private static function set_staging_mode( &$debug_data ) {
+		$debug_data['wcs_staging'] = array(
+			'name'    => _x( 'Subscriptions Mode', 'Live or Staging, Label on WooCommerce -> System Status page', 'woocommerce-subscriptions' ),
+			'note'    => '<strong>' . ( ( WC_Subscriptions::is_duplicate_site() ) ? _x( 'Staging', 'refers to staging site', 'woocommerce-subscriptions' ) :  _x( 'Live', 'refers to live site', 'woocommerce-subscriptions' ) ) . '</strong>',
+			'success' => ( WC_Subscriptions::is_duplicate_site() ) ? 0 : 1,
+		);
+	}
+
+	/**
+	 * List any Subscriptions template overrides.
+	 */
+	private static function set_theme_overrides( &$debug_data ) {
+		$theme_overrides = self::get_theme_overrides();
+
+		$debug_data['wcs_theme_overrides'] = array(
+			'name'      => _x( 'Subscriptions Template Theme Overrides', 'label for the system status page', 'woocommerce-subscriptions' ),
+			'data'      => $theme_overrides,
+		);
 	}
 
 	/**
@@ -166,11 +169,9 @@ class WCS_Admin_System_Status {
 	}
 
 	/**
-	 * Get a breakdown of Subscriptions per status.
-	 *
-	 * @return string
+	 * Add a breakdown of Subscriptions per status.
 	 */
-	private static function get_subscriptions_statuses() {
+	private static function set_subscription_statuses( &$debug_data ) {
 
 		$subscriptions_by_status        = (array) wp_count_posts( 'shop_subscription' );
 		$subscriptions_by_status_output = '';
@@ -181,15 +182,55 @@ class WCS_Admin_System_Status {
 			}
 		}
 
-		return $subscriptions_by_status_output;
+		$debug_data['wcs_subscriptions_by_status'] = array(
+			'name'      => _x( 'Subscription Statuses', 'label for the system status page', 'woocommerce-subscriptions' ),
+			'mark'      => '',
+			'mark_icon' => '',
+			'data'      => $subscriptions_by_status_output,
+		);
 	}
 
 	/**
-	 * Get a breakdown of Subscriptions per status per payment gateway.
-	 *
-	 * @return array
+	 * Include information about whether the store is linked to a WooCommerce account and whether they have an active WCS product key.
 	 */
-	private static function get_subscriptions_payment_gateway_data() {
+	private static function set_woocommerce_account_data( &$debug_data ) {
+
+		if ( class_exists( 'WC_Helper' ) ) {
+			$woocommerce_account_auth      = WC_Helper_Options::get( 'auth' );
+			$woocommerce_account_connected = ! empty( $woocommerce_account_auth );
+
+			$debug_data['wcs_woocommerce_account_connected'] = array(
+				'name'      => _x( 'WooCommerce Account Connected', 'label for the system status page', 'woocommerce-subscriptions' ),
+				'mark_icon' => $woocommerce_account_connected ? 'yes' : 'warning',
+				'note'      => $woocommerce_account_connected ? 'Yes' : 'No',
+				'success'   => $woocommerce_account_connected,
+			);
+
+			if ( $woocommerce_account_connected ) {
+				$woocommerce_account_subscriptions = WC_Helper::get_subscriptions();
+				$site_id                           = absint( $woocommerce_account_auth['site_id'] );
+
+				foreach ( $woocommerce_account_subscriptions as $subscription ) {
+					if ( isset( $subscription['product_id'] ) && self::WCS_PRODUCT_ID === $subscription['product_id'] ) {
+						$active = in_array( $site_id, $subscription['connections'] );
+
+						$debug_data['wcs_active_product_key'] = array(
+							'name'      => _x( 'Active Product Key', 'label for the system status page', 'woocommerce-subscriptions' ),
+							'mark_icon' => $active ? 'yes' : 'no',
+							'note'      => $active ? 'Yes' : 'No',
+							'success'   => $active,
+						);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Add a breakdown of subscriptions per payment gateway.
+	 */
+	private static function set_subscriptions_by_payment_gateway( &$debug_data ) {
 		global $wpdb;
 
 		$gateways = WC()->payment_gateways->get_available_payment_gateways();
@@ -207,26 +248,47 @@ class WCS_Admin_System_Status {
 
 			if ( isset( $gateways[ $payment_method ] ) ) {
 				$payment_method_name = $gateways[ $payment_method ]->method_title;
-				$supports            = $gateways[ $payment_method ]->supports;
 			} else {
 				$payment_method      = 'other';
 				$payment_method_name = 'Other';
-				$supports            = array( 'unknown' );
 			}
 
-			$key = 'wcs_payment_method_' . $payment_method;
+			$key = 'wcs_payment_method_subscriptions_by' . $payment_method;
 
-			if ( ! isset( $subscriptions_payment_gateway_data[ $key ] ) ) {
-				$subscriptions_payment_gateway_data[ $key ] = array(
+			if ( ! isset( $debug_data[ $key ] ) ) {
+				$debug_data[ $key ] = array(
 					'name' => $payment_method_name,
-					'data' => array( 'supports: ' . implode( ', ', $supports ) ),
+					'data' => array(),
 				);
 			}
 
-			$subscriptions_payment_gateway_data[ $key ]['data'][] = $subscription_status . ': ' . $result['count'];
+			$debug_data[ $key ]['data'][] = $subscription_status . ': ' . $result['count'];
 		}
+	}
 
-		return $subscriptions_payment_gateway_data;
+	/**
+	 * List the enabled payment gateways and the features they support.
+	 */
+	private static function set_subscriptions_payment_gateway_support( &$debug_data ) {
+		$gateways = WC()->payment_gateways->get_available_payment_gateways();
+
+		foreach ( $gateways as $gateway_id => $gateway ) {
+			$debug_data[ 'wcs_' . $gateway_id . '_feature_support' ] = array(
+				'name' => $gateway->method_title,
+				'data' => $gateway->supports,
+			);
+
+			if ( 'paypal' === $gateway_id ) {
+				$are_reference_transactions_enabled = WCS_PayPal::are_reference_transactions_enabled();
+
+				$debug_data['wcs_paypal_reference_transactions'] = array(
+					'name'      => _x( 'PayPal Reference Transactions Enabled', 'label for the system status page', 'woocommerce-subscriptions' ),
+					'mark_icon' => $are_reference_transactions_enabled ? 'yes' : 'warning',
+					'note'      => $are_reference_transactions_enabled ? 'Yes' : 'No',
+					'success'   => $are_reference_transactions_enabled,
+				);
+			}
+		}
 	}
 }
 WCS_Admin_System_Status::init();
