a1e182d16dde643b9af683ea305a599a63316800
James Allan <james.allan@live.com>
2018-06-08 15:41:39 +1000

    Only get visible grouped parents when determining an item's switchability

--- includes/class-wc-subscriptions-product.php
+++ includes/class-wc-subscriptions-product.php
@@ -1136,20 +1136,20 @@ class WC_Subscriptions_Product {
 	/**
 	 * Get a product's list of parent IDs which are a grouped type.
 	 *
-	 * Unlike @see WC_Subscriptions_Product::get_parent_ids(), this function will return parent products which still exist and are a grouped product.
+	 * Unlike @see WC_Subscriptions_Product::get_parent_ids(), this function will return parent products which still exist, are visible and are a grouped product.
 	 *
 	 * @param WC_Product The product object to get parents from.
 	 * @return array The product's grouped parent IDs.
 	 * @since 2.3.0
 	 */
-	public static function get_grouped_parent_product_ids( $product ) {
+	public static function get_visible_grouped_parent_product_ids( $product ) {
 		$parent_product_ids = self::get_parent_ids( $product );
 
 		// Verify that the parent products exist and are indeed grouped products
 		foreach ( $parent_product_ids as $index => $product_id ) {
 			$parent_product = wc_get_product( $product_id );
 
-			if ( ! is_a( $parent_product, 'WC_Product' ) || ! $parent_product->is_type( 'grouped' ) || 'trash' === wcs_get_objects_property( $parent_product, 'post_status' ) ) {
+			if ( ! is_a( $parent_product, 'WC_Product' ) || ! $parent_product->is_type( 'grouped' ) || 'publish' !== wcs_get_objects_property( $parent_product, 'post_status' ) ) {
 				unset( $parent_product_ids[ $index ] );
 			}
 		}
