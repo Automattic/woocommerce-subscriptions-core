bec689f0792bf7fb39864b6fa8490a14ed3b38a8
James Allan <james.allan@live.com>
2018-06-12 19:38:56 +1000

    [WC 2.6] Ensure a product has a post parent before trying to access it

--- includes/class-wc-subscriptions-product.php
+++ includes/class-wc-subscriptions-product.php
@@ -1119,7 +1119,7 @@ class WC_Subscriptions_Product {
 		global $wpdb;
 		$parent_product_ids = array();
 
-		if ( WC_Subscriptions::is_woocommerce_pre( '3.0' ) && $product->get_parent() ) {
+		if ( WC_Subscriptions::is_woocommerce_pre( '3.0' ) && isset( $product->post->post_parent ) ) {
 			$parent_product_ids[] = $product->get_parent();
 		} else {
 			$parent_product_ids = $wpdb->get_col( $wpdb->prepare(
