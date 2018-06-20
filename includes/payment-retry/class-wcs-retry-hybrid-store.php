1b0a62ca6e109bed7ed61f537569b9dc7675370b
Rommel Castro <rommelxcastro@gmail.com>
2018-06-20 00:46:49 -0600

    Initializes and save the sources for our hybrid store.

--- includes/payment-retry/class-wcs-retry-hybrid-store.php
+++ includes/payment-retry/class-wcs-retry-hybrid-store.php
@@ -15,12 +15,27 @@
  */
 class WCS_Retry_Hybrid_Store extends WCS_Retry_Store {
 	/**
+	 * Where we're saving/migrating our data.
+	 *
+	 * @var WCS_Retry_Store
+	 */
+	private static $destination_store;
+
+	/**
+	 * Where the data comes from.
+	 *
+	 * @var WCS_Retry_Store
+	 */
+	private static $source_store;
+
+	/**
 	 * Setup the class, if required
 	 *
-	 * @return null
+	 * @void
 	 */
 	public function init() {
-		// TODO: Implement init() method.
+		add_filter( 'init', array( self::destination_store(), 'init' ) );
+		add_filter( 'init', array( self::source_store(), 'init' ) );
 	}
 
 	/**
@@ -65,5 +80,51 @@ class WCS_Retry_Hybrid_Store extends WCS_Retry_Store {
 	 */
 	protected function get_retry_ids_for_order( $order_id ) {
 		// TODO: Implement get_retry_ids_for_order() method.
+	}
+
+	/**
+	 * Access the object used to interface with the destination store.
+	 *
+	 * @return WCS_Retry_Store
+	 */
+	public static function destination_store() {
+		if ( empty( self::$destination_store ) ) {
+			$class                   = self::get_destination_store_class();
+			self::$destination_store = new $class();
+		}
+
+		return self::$destination_store;
+	}
+
+	/**
+	 * Get the class used for instantiating retry storage via self::destination_store()
+	 *
+	 * @return mixed
+	 */
+	protected static function get_destination_store_class() {
+		return apply_filters( 'wcs_retry_destination_store_class', 'WCS_Retry_Database_Store' );
+	}
+
+	/**
+	 * Access the object used to interface with the source store.
+	 *
+	 * @return WCS_Retry_Store
+	 */
+	public static function source_store() {
+		if ( empty( self::$source_store ) ) {
+			$class              = self::get_source_store_class();
+			self::$source_store = new $class();
+		}
+
+		return self::$destination_store;
+	}
+
+	/**
+	 * Get the class used for instantiating retry storage via self::source_store()
+	 *
+	 * @return mixed
+	 */
+	protected static function get_source_store_class() {
+		return apply_filters( 'wcs_retry_source_store_class', 'WCS_Retry_Post_Store' );
 	}
 }
