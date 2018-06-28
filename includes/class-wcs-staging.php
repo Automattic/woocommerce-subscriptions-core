1bddef818ccbf0495a0e4f58b418164d137f7cf1
James Allan <james.allan@live.com>
2018-06-28 12:44:12 +1000

    Display a staging mode badge next to the Subscriptions submenu item
    For sites which have hidden the admin notice, this more minimal badge will flag admin users to the fact that their site is operating in staging mode.

--- includes/class-wcs-staging.php
+++ includes/class-wcs-staging.php
@@ -13,6 +13,7 @@ class WCS_Staging {
 	 */
 	public static function init() {
 		add_action( 'woocommerce_generated_manual_renewal_order', array( __CLASS__, 'maybe_record_staging_site_renewal' ) );
+		add_filter( 'woocommerce_register_post_type_subscription', array( __CLASS__, 'maybe_add_menu_badge' ) );
 	}
 
 	/**
@@ -32,5 +33,20 @@ class WCS_Staging {
 		if ( $renewal_order ) {
 			$renewal_order->add_order_note( __( 'Payment processing skipped - renewal order created under staging site lock.', 'woocommerce-subscriptions' ) );
 		}
+	}
+
+	/**
+	 * Add a badge to the Subscriptions submenu when a site is operating under a staging site lock.
+	 *
+	 * @param array $subscription_order_type_data The WC_Subscription register order type data.
+	 * @since 2.3.0
+	 */
+	public static function maybe_add_menu_badge( $subscription_order_type_data ) {
+
+		if ( isset( $subscription_order_type_data['labels']['menu_name'] ) && WC_Subscriptions::is_duplicate_site() ) {
+			$subscription_order_type_data['labels']['menu_name'] .= '<span class="update-plugins">' . esc_html__( 'staging', 'woocommerce-subscriptions' ) . '</span>';
+		}
+
+		return $subscription_order_type_data;
 	}
 }
