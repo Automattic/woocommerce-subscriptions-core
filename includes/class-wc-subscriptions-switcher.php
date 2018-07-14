862a0d8f7de383dd912126df4a9d20c818ad50d0
LCmry <lcrees.2014@gmail.com>
2018-07-14 08:17:29 -0700

    Replace while loop, which added time

--- includes/class-wc-subscriptions-switcher.php
+++ includes/class-wc-subscriptions-switcher.php
@@ -1409,11 +1409,9 @@ class WC_Subscriptions_Switcher {
 
 						// Find out how many days at the new price per day the customer would receive for the total amount already paid
 						// (e.g. if the customer paid $10 / month previously, and was switching to a $5 / week subscription, she has pre-paid 14 days at the new price)
-						$pre_paid_days = $new_total_paid = 0;
-
-						while ( $new_total_paid < $old_recurring_total ) {
-							$pre_paid_days++;
-							$new_total_paid = $pre_paid_days * $new_price_per_day;
+						$pre_paid_days = 0;
+						if ( $old_price_per_day != 0 ) {
+							$pre_paid_days = ceil( $old_total_paid / $new_price_per_day );
 						}
 
 						// If the total amount the customer has paid entitles her to more days at the new price than she has received, there is no gap payment, just shorten the pre-paid term the appropriate number of days
