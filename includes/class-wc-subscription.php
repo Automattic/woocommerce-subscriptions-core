708a39df7dd24617702f6853cc0fe5fdcf7a767a
James Allan <james.allan@live.com>
2018-07-16 16:39:57 +1000

    Add tax if requesting the line items signup fee inclusive of tax
    #2722 introduced a bug because it would subtract the tax if requesting prices exclusive of tax. The issue with doing that is that all amounts returned from order item get totals are already excluding tax so subtracting it again would lead to incorrect results.

--- includes/class-wc-subscription.php
+++ includes/class-wc-subscription.php
@@ -2132,17 +2132,13 @@ class WC_Subscription extends WC_Order {
 				$sign_up_fee = max( $order_line_total - $subscription_line_total, 0 );
 			}
 
-			if ( ! empty( $original_order_item ) && ! empty( $sign_up_fee ) ) {
+			// If prices don't inc tax, ensure that the sign up fee amount includes the tax.
+			if ( 'inclusive_of_tax' === $tax_inclusive_or_exclusive && ! empty( $original_order_item ) && ! empty( $sign_up_fee ) ) {
 				$sign_up_fee_proportion = $sign_up_fee / ( $original_order_item->get_total( 'edit' ) / $original_order_item->get_quantity( 'edit' ) );
 				$sign_up_fee_tax        = wc_round_tax_total( $original_order_item->get_total_tax( 'edit' ) * $sign_up_fee_proportion );
 
-				// If prices don't inc tax, ensure that the sign up fee amount includes the tax.
-				if ( 'inclusive_of_tax' === $tax_inclusive_or_exclusive && ! $this->get_prices_include_tax() ) {
-					$sign_up_fee += $sign_up_fee_tax;
-				// If prices inc tax and the request is for prices exclusive of tax, remove the taxes.
-				} elseif ( 'inclusive_of_tax' !== $tax_inclusive_or_exclusive && $this->get_prices_include_tax() ) {
-					$sign_up_fee -= $sign_up_fee_tax;
-				}
+				$sign_up_fee += $sign_up_fee_tax;
+				$sign_up_fee  = wc_format_decimal( $sign_up_fee, wc_get_price_decimals() );
 			}
 		}
 
