dd9551ba8e06b0c5350536077f0d4aaf97db33bf
Jorge A. Torres <jorge@prospress.com>
2018-09-17 13:14:00 -0300

    Prevent changing of product type when product has active subscriptions

--- includes/admin/class-wc-subscriptions-admin.php
+++ includes/admin/class-wc-subscriptions-admin.php
@@ -739,6 +739,8 @@ class WC_Subscriptions_Admin {
 					'bulkEditIntervalhMessage'  => __( 'Enter a new interval as a single number (e.g. to charge every 2nd month, enter 2):', 'woocommerce-subscriptions' ),
 					'bulkDeleteOptionLabel'     => __( 'Delete all variations without a subscription', 'woocommerce-subscriptions' ),
 					'oneTimeShippingCheckNonce' => wp_create_nonce( 'one_time_shipping' ),
+					'productHasSubscriptions'   => wcs_get_subscriptions_for_product( $post->ID ) ? 'yes' : 'no',
+					'productTypeWarning'        => __( 'Product type can not be changed because this product is associated with active subscriptions', 'woocommerce-subscriptions' ),
 				);
 			} else if ( 'edit-shop_order' == $screen->id ) {
 				$script_params = array(
