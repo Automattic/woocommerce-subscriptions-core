5f77da1fa0eb271befa8de9101b5d42992fefd21
Rommel Castro <rommelxcastro@gmail.com>
2018-10-24 00:06:41 +0700

    sort results to keep consistency between cached results and queried results.

--- includes/data-stores/class-wcs-customer-store-cached-cpt.php
+++ includes/data-stores/class-wcs-customer-store-cached-cpt.php
@@ -87,6 +87,9 @@ class WCS_Customer_Store_Cached_CPT extends WCS_Customer_Store_CPT implements WC
 			$this->update_subscription_id_cache( $user_id, $subscription_ids );
 		}
 
+		// Sort results in order to keep consistency between cached results and queried results.
+		rsort( $subscription_ids );
+
 		return $subscription_ids;
 	}
 
