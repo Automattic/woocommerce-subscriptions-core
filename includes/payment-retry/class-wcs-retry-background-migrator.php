10eb94c738c140ede9330cc05899bd7905e91bdb
Rommel Castro <rommelxcastro@gmail.com>
2018-10-31 20:13:53 +0700

    Update since tags in WCS_Retry_Background_Migrator

--- includes/payment-retry/class-wcs-retry-background-migrator.php
+++ includes/payment-retry/class-wcs-retry-background-migrator.php
@@ -6,7 +6,7 @@
  * @category    Class
  * @package     WooCommerce Subscriptions
  * @subpackage  WCS_Retry_Backgound_Migrator
- * @since       2.4
+ * @since       2.4.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Class WCS_Retry_Background_Migrator.
  *
  * Updates our retries on background.
- * @since 2.4
+ * @since 2.4.0
  */
 class WCS_Retry_Background_Migrator extends WCS_Background_Upgrader {
 	/**
@@ -45,6 +45,8 @@ class WCS_Retry_Background_Migrator extends WCS_Background_Upgrader {
 	 * construct.
 	 *
 	 * @param WC_Logger_Interface $logger The WC_Logger instance.
+	 *
+	 * @since 2.4.0
 	 */
 	public function __construct( WC_Logger_Interface $logger ) {
 		$this->scheduled_hook = 'wcs_retries_migration_hook';
@@ -64,7 +66,7 @@ class WCS_Retry_Background_Migrator extends WCS_Background_Upgrader {
 	 * Get the items to be updated, if any.
 	 *
 	 * @return array An array of items to update, or empty array if there are no items to update.
-	 * @since 2.4
+	 * @since 2.4.0
 	 */
 	protected function get_items_to_update() {
 		return $this->source_store->get_retries();
@@ -76,7 +78,7 @@ class WCS_Retry_Background_Migrator extends WCS_Background_Upgrader {
 	 * @param WCS_Retry $retry The item to update.
 	 *
 	 * @return int|null
-	 * @since 2.4
+	 * @since 2.4.0
 	 */
 	protected function update_item( $retry ) {
 		try {
@@ -104,6 +106,7 @@ class WCS_Retry_Background_Migrator extends WCS_Background_Upgrader {
 
 	/**
 	 * Unscheduled the instance's hook in Action Scheduler
+	 * @since 2.4.1
 	 */
 	protected function unschedule_background_updates() {
 		parent::unschedule_background_updates();
