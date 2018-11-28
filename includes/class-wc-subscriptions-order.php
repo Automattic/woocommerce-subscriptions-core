0eaaee49a1a748e8c54bc08afce7679fe1116114
Rommel Castro <rommelxcastro@gmail.com>
2018-11-28 00:00:34 -0500

    Removes post WC 2.6 block.

--- includes/class-wc-subscriptions-order.php
+++ includes/class-wc-subscriptions-order.php
@@ -372,11 +372,6 @@ class WC_Subscriptions_Order {
 			$thank_you_message            = '<p>' . _n( 'Your subscription will be activated when payment clears.', 'Your subscriptions will be activated when payment clears.', $subscription_count, 'woocommerce-subscriptions' ) . '</p>';
 			$my_account_subscriptions_url = get_permalink( wc_get_page_id( 'myaccount' ) );
 
-			// Post WC 2.6 link directly to the My Account subscriptions endpoint
-			if ( ! WC_Subscriptions::is_woocommerce_pre( '2.6' ) ) {
-				$my_account_subscriptions_url = wc_get_endpoint_url( 'subscriptions', '', wc_get_page_permalink( 'myaccount' ) );
-			}
-
 			// translators: placeholders are opening and closing link tags
 			$thank_you_message .= '<p>' . sprintf( _n( 'View the status of your subscription in %syour account%s.', 'View the status of your subscriptions in %syour account%s.', $subscription_count, 'woocommerce-subscriptions' ), '<a href="' . $my_account_subscriptions_url . '">', '</a>' ) . '</p>';
 			echo wp_kses( apply_filters( 'woocommerce_subscriptions_thank_you_message', $thank_you_message, $order_id ), array( 'a' => array( 'href' => array(), 'title' => array() ), 'p' => array(), 'em' => array(), 'strong' => array() ) );
