b743cde2abcfd316440f288dff29cb1d7c53e27a
Rommel Castro <rommelxcastro@gmail.com>
2019-02-12 11:26:44 -0600

    Adds WCS_Permalink_Manager

---
+++ includes/class-wcs-permalink-manager.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * WooCommerce Subscriptions Permalink Manager
+ *
+ * Handles and allows WCS related permalinks/endpoints.
+ *
+ * @package  WooCommerce Subscriptions
+ * @category Class
+ * @author   Prospress
+ * @since    2.5.3
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Class WCS_Permalink_Manager
+ */
+class WCS_Permalink_Manager {
+	/**
+	 * Hooks.
+	 *
+	 * @since 2.5.3
+	 */
+	public static function init() {
+	}
+}
