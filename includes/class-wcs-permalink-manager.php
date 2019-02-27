450dfea934b06a339b64bad902a5bd07abc9336b
Rommel Castro <rommelxcastro@gmail.com>
2019-02-26 19:59:39 -0600

    Adds notice

--- includes/class-wcs-permalink-manager.php
+++ includes/class-wcs-permalink-manager.php
@@ -19,6 +19,14 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class WCS_Permalink_Manager {
 	/**
+	 * If the notice has been trigger, set to true to avoid duplicate notices.
+	 *
+	 * @var bool
+	 * @since 2.5.3
+	 */
+	protected static $notice_triggered = false;
+
+	/**
 	 * The options saved in DB related to permalinks.
 	 *
 	 * @var array
@@ -64,5 +72,23 @@ class WCS_Permalink_Manager {
 		}
 
 		return $value;
+	}
+
+	/**
+	 * Display a warning informing that the endpoints changes has been ignored.
+	 *
+	 * @since 2.5.3
+	 */
+	protected static function show_duplicate_permalink_notice() {
+		if ( ! self::$notice_triggered ) {
+			self::$notice_triggered = true;
+			$notice                 = new WCS_Admin_Notice( 'error' );
+
+			$notice->set_simple_content(
+				// translators: 1$-2$: opening and closing <strong> tags.
+				sprintf( esc_html__( 'Error saving Subscriptions endpoints: %1$sSubscriptions%2$s, %1$sView subscription%2$s and %1$sSubscription payment method%2$s cannot be the same. The changes have been reverted.', 'woocommerce-subscriptions' ), '<strong>', '</strong>' )
+			);
+			$notice->display();
+		}
 	}
 }
