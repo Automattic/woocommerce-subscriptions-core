baed419dffd90a339dca44e4c8fa9e55490d00c3
James Allan <james@prospress.com>
2019-03-19 15:44:16 +1000

    Refactor the subscription receipt to a separate template

--- includes/class-wc-subscriptions-change-payment-gateway.php
+++ includes/class-wc-subscriptions-change-payment-gateway.php
@@ -168,44 +168,8 @@ class WC_Subscriptions_Change_Payment_Gateway {
 			 */
 			do_action( 'wcs_before_replace_pay_shortcode', $subscription );
 
-			if ( $subscription->get_id() == absint( $wp->query_vars['order-pay'] ) && $subscription->get_order_key() == $subscription_key ) {
-
-				?>
-			<div class="woocommerce">
-				<ul class="order_details">
-					<li class="order">
-						<?php
-						// translators: placeholder is the subscription order number wrapped in <strong> tags
-						echo wp_kses( sprintf( esc_html__( 'Subscription Number: %s', 'woocommerce-subscriptions' ), '<strong>' . esc_html( $subscription->get_order_number() ) . '</strong>' ), array( 'strong' => true ) );
-						?>
-					</li>
-					<li class="date">
-						<?php
-						// translators: placeholder is the subscription's next payment date (either human readable or normal date) wrapped in <strong> tags
-						echo wp_kses( sprintf( esc_html__( 'Next Payment Date: %s', 'woocommerce-subscriptions' ), '<strong>' . esc_html( $subscription->get_date_to_display( 'next_payment' ) ) . '</strong>' ), array( 'strong' => true ) );
-						?>
-					</li>
-					<li class="total">
-						<?php
-						// translators: placeholder is the formatted total to be paid for the subscription wrapped in <strong> tags
-						echo wp_kses_post( sprintf( esc_html__( 'Total: %s', 'woocommerce-subscriptions' ), '<strong>' . $subscription->get_formatted_order_total() . '</strong>' ) );
-						?>
-					</li>
-					<?php if ( $subscription->get_payment_method_title() ) : ?>
-						<li class="method">
-							<?php
-							// translators: placeholder is the display name of the payment method
-							echo wp_kses( sprintf( esc_html__( 'Payment Method: %s', 'woocommerce-subscriptions' ), '<strong>' . esc_html( $subscription->get_payment_method_to_display() ) . '</strong>' ), array( 'strong' => true ) );
-							?>
-						</li>
-					<?php endif; ?>
-				</ul>
-
-				<?php do_action( 'woocommerce_receipt_' . $subscription->get_payment_method(), $subscription->get_id() ); ?>
-
-				<div class="clear"></div>
-				<?php
-
+			if ( $subscription->get_id() === absint( $wp->query_vars['order-pay'] ) && $subscription->get_order_key() === $subscription_key ) {
+				WCS_Template_Loader::get_subscription_receipt_template( $subscription );
 			} else {
 				wc_add_notice( __( 'Sorry, this subscription change payment method request is invalid and cannot be processed.', 'woocommerce-subscriptions' ), 'error' );
 			}
