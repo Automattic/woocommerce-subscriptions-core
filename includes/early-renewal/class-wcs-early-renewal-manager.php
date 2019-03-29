a50d94699c7fbc9b1510c22a176a820265b0f89d
James Allan <james@prospress.com>
2019-03-29 15:49:05 +1000

    Refactor early renewal date calculation and updating for reuse

--- includes/early-renewal/class-wcs-early-renewal-manager.php
+++ includes/early-renewal/class-wcs-early-renewal-manager.php
@@ -107,4 +107,32 @@ class WCS_Early_Renewal_Manager {
 		$enabled = self::is_early_renewal_enabled() && 'yes' === get_option( self::$via_modal_setting_id, 'no' );
 		return apply_filters( 'wcs_is_early_renewal_via_modal_enabled', $enabled );
 	}
+
+	/**
+	 * Gets the dates which need to be updated after an early renewal is processed.
+	 *
+	 * @since 2.6.0
+	 *
+	 * @param WC_Subscription $subscription The subscription to calculate the dates for.
+	 * @return array The subscription dates which need to be updated. For example array( $date_type => $mysql_form_date_string ).
+	 */
+	public static function get_dates_to_update( $subscription ) {
+		$next_payment_time = $subscription->get_time( 'next_payment' );
+		$dates_to_update   = array();
+
+		if ( $next_payment_time > 0 && $next_payment_time > current_time( 'timestamp', true ) ) {
+			$next_payment_timestamp = wcs_add_time( $subscription->get_billing_interval(), $subscription->get_billing_period(), $next_payment_time );
+
+			if ( $subscription->get_time( 'end' ) === 0 || $next_payment_timestamp < $subscription->get_time( 'end' ) ) {
+				$dates_to_update['next_payment'] = gmdate( 'Y-m-d H:i:s',  $next_payment_timestamp );
+			} else {
+				// Delete the next payment date if the calculated next payment date occurs after the end date.
+				$dates_to_update['next_payment'] = 0;
+			}
+		} elseif ( $subscription->get_time( 'end' ) > 0 ) {
+			$dates_to_update['end'] = gmdate( 'Y-m-d H:i:s', wcs_add_time( $subscription->get_billing_interval(), $subscription->get_billing_period(), $subscription->get_time( 'end' ) ) );
+		}
+
+		return $dates_to_update;
+	}
 }
