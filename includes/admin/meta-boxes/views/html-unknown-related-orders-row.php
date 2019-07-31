1c93f2be4b20af4f678567fb3ccf85bdfaa09d86
James Allan <james.allan@automattic.com>
2019-07-31 11:06:54 +1000

    Remove link symbol and turn (?) tooltip into link

--- includes/admin/meta-boxes/views/html-unknown-related-orders-row.php
+++ includes/admin/meta-boxes/views/html-unknown-related-orders-row.php
@@ -15,8 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<td>
 		<?php echo sprintf( esc_html_x( '#%s', 'hash before order number', 'woocommerce-subscriptions' ), esc_html( $order_id ) ); ?>
 		<div class="wcs-unknown-order-info-wrapper">
-			<?php echo wcs_help_tip( sprintf( "This %s couldn't be loaded from the database. %s Click %s to learn more.", $relationship, '</br>', '<span class="dashicons dashicons-external"></span>' ) ); ?>
-			<a href="https://docs.woocommerce.com/document/subscriptions/orders/#section-8"><span class="wcs-unknown-order-link dashicons dashicons-external"></span></a>
+			<a href="https://docs.woocommerce.com/document/subscriptions/orders/#section-8"><?php echo wcs_help_tip( sprintf( "This %s couldn't be loaded from the database. %s Click to learn more.", $relationship, '</br>' ) ); ?></a>
 		</div>
 	</td>
 	<td><?php echo esc_html( $relationship ); ?></td>
