2911275748650b147628da32757d11ffca443dba
LC <lc@LCs-MacBook-Pro.local>
2020-04-22 09:44:56 -0700

    Save payment method changes on non-manual gateways

--- includes/class-wcs-change-payment-method-admin.php
+++ includes/class-wcs-change-payment-method-admin.php
@@ -135,8 +135,8 @@ class WCS_Change_Payment_Method_Admin {
 			WC_Subscriptions_Payment_Gateways::trigger_gateway_status_updated_hook( $subscription, $gateway_status );
 		}
 
-		// Update the payment method only if it has changed.
-		if ( ( $subscription->is_manual() && 'manual' !== $payment_method ) || ( ! $subscription->is_manual() && $subscription->get_payment_method() !== $payment_method ) ) {
+		// Update the payment method for manual only if it has changed.
+		if ( ( $subscription->is_manual() && 'manual' !== $payment_method ) || ! $subscription->is_manual() ) {
 			$subscription->set_payment_method( $payment_gateway, $payment_method_meta );
 			$subscription->save();
 		}
