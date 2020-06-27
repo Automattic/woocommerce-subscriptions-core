522ab987d0ea487db8bb1ef891b7f6dead9682d8
Omkar Bhagat <codestor4@gmail.com>
2020-06-27 19:47:34 +0000

    Fetch the notice message correctly
    Before we try to fetch notice msg from $notice but the actual notice msg is located inside $notice['notice'].

--- includes/class-wc-subscriptions-change-payment-gateway.php
+++ includes/class-wc-subscriptions-change-payment-gateway.php
@@ -185,7 +185,7 @@ class WC_Subscriptions_Change_Payment_Gateway {
 			// Re-add all the notices that would have been displayed but have now been cleared from the output.
 			foreach ( self::$notices as $notice_type => $notices ) {
 				foreach ( $notices as $notice ) {
-					wc_add_notice( $notice, $notice_type );
+					wc_add_notice( $notice['notice'], $notice_type );
 				}
 			}
 
