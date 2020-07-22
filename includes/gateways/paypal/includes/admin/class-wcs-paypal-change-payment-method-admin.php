3087241f1424e1a91bc6f2be97e7a3e9ba46019c
Jorge A. Torres <jorge.torres@automattic.com>
2020-07-22 18:05:18 -0300

    Fix WordPress.Arrays.* smells in includes/gateways/paypal/

--- includes/gateways/paypal/includes/admin/class-wcs-paypal-change-payment-method-admin.php
+++ includes/gateways/paypal/includes/admin/class-wcs-paypal-change-payment-method-admin.php
@@ -56,8 +56,8 @@ class WCS_PayPal_Change_Payment_Method_Admin {
 		$payment_meta['paypal'] = array(
 			'post_meta' => array(
 				'_paypal_subscription_id' => array(
-					'value' => $subscription_id,
-					'label' => $label,
+					'value'    => $subscription_id,
+					'label'    => $label,
 					'disabled' => $disabled,
 				),
 			),
