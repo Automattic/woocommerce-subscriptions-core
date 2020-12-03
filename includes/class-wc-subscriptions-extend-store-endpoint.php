8f734b58b777833fba36513914498d0a737b950d
Seghir Nadir <nadir.seghir@gmail.com>
2020-12-03 14:03:12 +0100

    add phpcs

--- includes/class-wc-subscriptions-extend-store-endpoint.php
+++ includes/class-wc-subscriptions-extend-store-endpoint.php
@@ -45,12 +45,14 @@ class WC_Subscriptions_Extend_Store_Endpoint {
 	 */
 	public static function extend_store() {
 
-		self::$extend->register_endpoint_data( [
-			'endpoint'  => CartItemSchema::IDENTIFIER,
-			'namespace' => self::IDENTIFIER,
-			'data_callback' => array( 'WC_Subscriptions_Extend_Store_Endpoint', 'extend_cart_item_data' ),
-			'schema_callback' => array( 'WC_Subscriptions_Extend_Store_Endpoint', 'extend_cart_item_schema' )
-		] );
+		self::$extend->register_endpoint_data(
+			array(
+				'endpoint'        => CartItemSchema::IDENTIFIER,
+				'namespace'       => self::IDENTIFIER,
+				'data_callback'   => array( 'WC_Subscriptions_Extend_Store_Endpoint', 'extend_cart_item_data' ),
+				'schema_callback' => array( 'WC_Subscriptions_Extend_Store_Endpoint', 'extend_cart_item_schema' ),
+			)
+		);
 	}
 
 	/**
@@ -62,18 +64,18 @@ class WC_Subscriptions_Extend_Store_Endpoint {
 	 */
 	public static function extend_cart_item_data( $cart_item ) {
 
-		$product = $cart_item['data'];
-		$item_data = [];
+		$product   = $cart_item['data'];
+		$item_data = array();
 
 		if ( $product->get_type() === 'subscription' || $product->get_type() === 'subscription_variation' ) {
-			$item_data = [
-				"billing_period"      => WC_Subscriptions_Product::get_period( $product ),
-				"billing_interval"    => (int) WC_Subscriptions_Product::get_interval( $product ),
-				"subscription_length" => (int) WC_Subscriptions_Product::get_length( $product ),
-				"trial_length"        => (int) WC_Subscriptions_Product::get_trial_length( $product ),
-				"trial_period"        => WC_Subscriptions_Product::get_trial_period( $product ),
-				"sign_up_fees"         => self::prepare_money_response( WC_Subscriptions_Product::get_sign_up_fee( $product ), wc_get_price_decimals() ),
-			];
+			$item_data = array(
+				'billing_period'      => WC_Subscriptions_Product::get_period( $product ),
+				'billing_interval'    => (int) WC_Subscriptions_Product::get_interval( $product ),
+				'subscription_length' => (int) WC_Subscriptions_Product::get_length( $product ),
+				'trial_length'        => (int) WC_Subscriptions_Product::get_trial_length( $product ),
+				'trial_period'        => WC_Subscriptions_Product::get_trial_period( $product ),
+				'sign_up_fees'        => self::prepare_money_response( WC_Subscriptions_Product::get_sign_up_fee( $product ), wc_get_price_decimals() ),
+			);
 		}
 
 		return $item_data;
@@ -86,44 +88,44 @@ class WC_Subscriptions_Extend_Store_Endpoint {
 	 * @return array $item_data Registered schema.
 	 */
 	public static function extend_cart_item_schema() {
-		return [
-			'billing_period'            => [
+		return array(
+			'billing_period'      => array(
 				'description' => __( 'Billing period for the subscription.', 'woocommerce-subscriptions' ),
 				'type'        => 'string',
 				'enum'        => array_keys( wcs_get_subscription_period_strings() ),
 				'context'     => array( 'view', 'edit' ),
-			],
-			'billing_interval'          => [
+			),
+			'billing_interval'    => array(
 				'description' => __( 'The number of billing periods between subscription renewals.', 'woocommerce-subscriptions' ),
 				'type'        => 'integer',
 				'context'     => array( 'view', 'edit' ),
-			],
-			"subscription_length"          => [
+			),
+			'subscription_length' => array(
 				'description' => __( 'Subscription Product length.', 'woocommerce-subscriptions' ),
 				'type'        => 'integer',
-				'context'     => [ 'view', 'edit' ],
+				'context'     => array( 'view', 'edit' ),
 				'readonly'    => true,
-			],
-			"trial_period"    => [
+			),
+			'trial_period'        => array(
 				'description' => __( 'Subscription Product trial period.', 'woocommerce-subscriptions' ),
 				'type'        => 'string',
 				'enum'        => array_keys( wcs_get_subscription_period_strings() ),
-				'context'     => [ 'view', 'edit' ],
+				'context'     => array( 'view', 'edit' ),
 				'readonly'    => true,
-			],
-			"trial_length"    => [
+			),
+			'trial_length'        => array(
 				'description' => __( 'Subscription Product trial interval.', 'woocommerce-subscriptions' ),
 				'type'        => 'integer',
-				'context'     => [ 'view', 'edit' ],
+				'context'     => array( 'view', 'edit' ),
 				'readonly'    => true,
-			],
-			"sign_up_fees"            => [
+			),
+			'sign_up_fees'        => array(
 				'description' => __( 'Subscription Product Signup fees.', 'woocommerce-subscriptions' ),
 				'type'        => 'string',
-				'context'     => [ 'view', 'edit' ],
+				'context'     => array( 'view', 'edit' ),
 				'readonly'    => true,
-			],
-		];
+			),
+		);
 	}
 
 	/**
