ef3be081077e829ae523d7eccfd7979041c4fabc
Thomas Roberts <thomas.roberts@automattic.com>
2020-12-07 12:53:45 +0000

    Add comments to WCS_Blocks_Integration class

--- includes/class-wcs-blocks-integration.php
+++ includes/class-wcs-blocks-integration.php
@@ -1,4 +1,6 @@
 <?php
+use Automattic\WooCommerce\Blocks\Assets\AssetDataRegistry;
+
 /**
  * Class for integrating with WooCommerce Blocks
  *
@@ -6,16 +8,28 @@
  * @author  WooCommerce
  * @since   WCBLOCKS-DEV
  */
-use Automattic\WooCommerce\Blocks\Assets\AssetDataRegistry;
 class WCS_Blocks_Integration {
 
-	protected $registry;
+	/**
+	 * The AssetDataRegistry that holds all of the data that should be output to the front-end.
+	 * We can insert items here using the add_data method of this class.
+	 *
+	 * @var AssetDataRegistry Holds the data we want to output onto the front-end.
+	 */
+	protected AssetDataRegistry $registry;
 
-	public function __construct( $registry ) {
+	/**
+	 * WCS_Blocks_Integration constructor.
+	 *
+	 * @param AssetDataRegistry $registry The registry that data from this plugin should be added to.
+	 */
+	public function __construct( AssetDataRegistry $registry ) {
 		$this->registry = $registry;
-		$this->add_data();
 	}
 
+	/**
+	 * Populate the registry with the predefined data.
+	 */
 	public function add_data() {
 		$this->registry->add(
 			'woocommerce-subscriptions-blocks',
