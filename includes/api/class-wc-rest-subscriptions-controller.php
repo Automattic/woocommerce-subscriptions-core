d4372cc98d8e0f7682271def5a9a28bc0cfa6689
Romario <romarioraff@gmail.com>
2021-06-18 16:55:50 -0500

    Add woocommerce_rest_pre_insert_{->post_type}_object filter

--- includes/api/class-wc-rest-subscriptions-controller.php
+++ includes/api/class-wc-rest-subscriptions-controller.php
@@ -331,9 +331,17 @@ class WC_REST_Subscriptions_Controller extends WC_REST_Orders_Controller {
 			}
 		}
 
-		$subscription->save();
-
-		return $subscription;
+		/**
+		 * Filters an object before it is inserted via the REST API.
+		 *
+		 * The dynamic portion of the hook name, `$this->post_type`,
+		 * refers to the object type slug.
+		 *
+		 * @param WC_Data         $subscription   Object object.
+		 * @param WP_REST_Request $request  Request object.
+		 * @param bool            $creating If is creating a new object.
+		 */
+		return apply_filters( "woocommerce_rest_pre_insert_{$this->post_type}_object", $subscription, $request, $creating );
 	}
 
 	/**
