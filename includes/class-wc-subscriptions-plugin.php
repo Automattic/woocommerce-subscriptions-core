ee7c9ee0bf04b998278ae09ea2d69e4f4e2c316b
mattallan <matt@prospress.com>
2021-06-23 15:16:48 +1000

    Move initing switch classes outside of base plugin

--- includes/class-wc-subscriptions-plugin.php
+++ includes/class-wc-subscriptions-plugin.php
@@ -9,4 +9,15 @@
 defined( 'ABSPATH' ) || exit;
 
 class WC_Subscriptions_Plugin extends WC_Subscriptions_Base_Plugin {
+
+	/**
+	 * Initialise the WC Subscriptions plugin.
+	 *
+	 * @since 4.0.0
+	 */
+	public function init() {
+		parent::init();
+		WC_Subscriptions_Switcher::init();
+		new WCS_Cart_Switch();
+	}
 }
