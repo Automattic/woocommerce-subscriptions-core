e30ee9f7cdf256c2070b7931e7071fb7e4fd7061
James Allan <james.allan@automattic.com>
2021-07-07 14:11:30 +1000

    Move the drip download content feature setting

--- includes/class-wcs-drip-downloads-manager.php
+++ includes/class-wcs-drip-downloads-manager.php
@@ -17,6 +17,7 @@ class WCS_Drip_Downloads_Manager {
 	 */
 	public static function init() {
 		add_filter( 'woocommerce_process_product_file_download_paths_grant_access_to_new_file', array( __CLASS__, 'maybe_revoke_immediate_access' ), 10, 4 );
+		add_filter( 'woocommerce_subscription_settings', array( __CLASS__, 'add_setting' ) );
 	}
 
 	/**
@@ -48,5 +49,28 @@ class WCS_Drip_Downloads_Manager {
 		}
 
 		return $grant_access;
+	}
+
+	/**
+	 * Adds the Drip Downloadable Content setting.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @param array $settings The WC Subscriptions settings array.
+	 * @return array Settings.
+	 */
+	public static function add_setting( $settings ) {
+		$setting = array(
+			'name'     => __( 'Drip Downloadable Content', 'woocommerce-subscriptions' ),
+			'desc'     => __( 'Enable dripping for downloadable content on subscription products.', 'woocommerce-subscriptions' ),
+			'id'       => WC_Subscriptions_Admin::$option_prefix . '_drip_downloadable_content_on_renewal',
+			'default'  => 'no',
+			'type'     => 'checkbox',
+			// translators: %s is a line break.
+			'desc_tip' => sprintf( __( 'Enabling this grants access to new downloadable files added to a product only after the next renewal is processed.%sBy default, access to new downloadable files added to a product is granted immediately to any customer that has an active subscription with that product.', 'woocommerce-subscriptions' ), '<br />' ),
+		);
+
+		WC_Subscriptions_Admin::insert_setting_after( $settings, WC_Subscriptions_Admin::$option_prefix . '_miscellaneous', $setting );
+		return $settings;
 	}
 }
