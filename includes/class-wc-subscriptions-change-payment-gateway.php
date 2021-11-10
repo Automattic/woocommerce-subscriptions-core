ed01a23ee049d70a11b8c3714995842ba4be78f4
Bruce Aldridge <bruce@incode.co.nz>
2021-11-11 11:37:13 +1300

    #3268 Update confusing subscription note

--- includes/class-wc-subscriptions-change-payment-gateway.php
+++ includes/class-wc-subscriptions-change-payment-gateway.php
@@ -533,8 +533,8 @@ class WC_Subscriptions_Change_Payment_Gateway {
 			$new_payment_method_title = (string) apply_filters( 'woocommerce_subscription_note_new_payment_method_title', $new_payment_method_title, $new_payment_method, $subscription );
 
 			// Log change on order
-			// translators: 1: old payment title (now unused, placement kept for backwards compatibility), 2: new payment title.
-			$subscription->add_order_note( sprintf( _x( 'Payment method changed to "%2$s" by the subscriber from their account page.', '%1$s: old payment title, %2$s: new payment title', 'woocommerce-subscriptions' ), $old_payment_method_title, $new_payment_method_title ) );
+			// translators: 1: old payment title, 2: new payment title.
+			$subscription->add_order_note( sprintf( _x( 'Payment method changed from "%1$s" to "%2$s" by the subscriber.', '%1$s: old payment title, %2$s: new payment title', 'woocommerce-subscriptions' ), $old_payment_method_title, $new_payment_method_title ) );
 
 			$subscription->save();
 
