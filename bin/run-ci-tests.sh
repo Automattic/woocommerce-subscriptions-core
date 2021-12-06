102fd82ab60d9de778df77cc75c048ac3426c285
Bruce Aldridge <bruce@incode.co.nz>
2021-12-07 12:38:33 +1300

    Remove sarb when running CI to prevent syntax errors on php7.0

--- bin/run-ci-tests.sh
+++ bin/run-ci-tests.sh
@@ -4,8 +4,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-composer self-update 2.0.6
-  && composer remove --dev "dave-liddament/sarb" # sarb creates problems on php 7.0
+# Install dependencies and remove sarb as it creates problems on php 7.0
+composer self-update 2.0.6 \
+  && composer remove --dev "dave-liddament/sarb" \
   && composer install --no-progress
 sudo systemctl start mysql.service
 bash bin/install-wp-tests.sh woocommerce_test root root localhost $WP_VERSION $WC_VERSION false
