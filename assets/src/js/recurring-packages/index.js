e40bb01a454fdc080b7497f43d8c9410681ad4f5
Shendy <73803630+shendy-a8c@users.noreply.github.com>
2021-12-14 16:12:46 +0700

    Update JSDoc for SubscriptionsRecurringPackages' parameters.

--- assets/src/js/recurring-packages/index.js
+++ assets/src/js/recurring-packages/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { useMemo } from '@wordpress/element';
+import { useMemo } from "@wordpress/element";
 
 /**
  * This component is responsible for rending recurring shippings.
@@ -17,47 +17,47 @@ import { useMemo } from '@wordpress/element';
  * @param {boolean}  props.showItems             If shipping rates should show items inside them.
  * @param {Element}  props.noResultsMessage      Message shown when no rate are found.
  * @param {Function} props.renderOption          Function that decides how rates are going to render.
- * @param {Function} props.useSelectShippingRate useSelectShippingRate hook.
+ * @param {Function} props.components            Components.
  */
 export const SubscriptionsRecurringPackages = ({
-	extensions,
-	collapsible,
-	collapse,
-	showItems,
-	noResultsMessage,
-	renderOption,
-	components,
+  extensions,
+  collapsible,
+  collapse,
+  showItems,
+  noResultsMessage,
+  renderOption,
+  components,
 }) => {
-	const { subscriptions = [] } = extensions;
-	const { ShippingRatesControlPackage } = components;
+  const { subscriptions = [] } = extensions;
+  const { ShippingRatesControlPackage } = components;
 
-	// Flatten all packages from recurring carts.
-	const packages = useMemo(
-		() =>
-			Object.values(subscriptions)
-				.map((recurringCart) => recurringCart.shipping_rates)
-				.filter(Boolean)
-				.flat(),
-		[subscriptions]
-	);
-	const shouldCollapse = useMemo(() => packages.length > 1 || collapse, [
-		packages.length,
-		collapse,
-	]);
-	const shouldShowItems = useMemo(() => packages.length > 1 || showItems, [
-		packages.length,
-		showItems,
-	]);
-	return packages.map(({ package_id: packageId, ...packageData }) => (
-		<ShippingRatesControlPackage
-			key={packageId}
-			packageId={packageId}
-			packageData={packageData}
-			collapsible={collapsible}
-			collapse={shouldCollapse}
-			showItems={shouldShowItems}
-			noResultsMessage={noResultsMessage}
-			renderOption={renderOption}
-		/>
-	));
+  // Flatten all packages from recurring carts.
+  const packages = useMemo(
+    () =>
+      Object.values(subscriptions)
+        .map((recurringCart) => recurringCart.shipping_rates)
+        .filter(Boolean)
+        .flat(),
+    [subscriptions]
+  );
+  const shouldCollapse = useMemo(() => packages.length > 1 || collapse, [
+    packages.length,
+    collapse,
+  ]);
+  const shouldShowItems = useMemo(() => packages.length > 1 || showItems, [
+    packages.length,
+    showItems,
+  ]);
+  return packages.map(({ package_id: packageId, ...packageData }) => (
+    <ShippingRatesControlPackage
+      key={packageId}
+      packageId={packageId}
+      packageData={packageData}
+      collapsible={collapsible}
+      collapse={shouldCollapse}
+      showItems={shouldShowItems}
+      noResultsMessage={noResultsMessage}
+      renderOption={renderOption}
+    />
+  ));
 };
