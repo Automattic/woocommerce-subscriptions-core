350733a1a1a39fca4698f5a926d6155699cbb2a7
Shendy <73803630+shendy-a8c@users.noreply.github.com>
2021-12-16 12:35:07 +0700

    test

--- bin/run-ci-tests.sh
+++ bin/run-ci-tests.sh
@@ -7,7 +7,9 @@ IFS=$'\n\t'
 # Install dependencies and remove sarb as it creates problems on php 7.0
 composer self-update 2.0.6 \
   && composer remove --dev "dave-liddament/sarb" \
-  && composer remove --dev "phpDocumentor" \
+  && composer remove --dev "phpdocumentor/reflection-docblock" \
+  && composer remove --dev "phpdocumentor/type-resolver" \
+  && composer remove --dev "doctrine/instantiator" \
   && composer install --no-progress
 sudo systemctl start mysql.service
 bash bin/install-wp-tests.sh woocommerce_test root root localhost $WP_VERSION $WC_VERSION false
