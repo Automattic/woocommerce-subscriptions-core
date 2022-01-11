9abc0194a2cc9e232acd9cda8784356c4174e555
James Allan <james.allan@automattic.com>
2022-01-11 12:01:51 +1000

    Add support for payment gateways to return the minumum that can be processed in recurring transactions

--- includes/gateways/class-wc-subscriptions-gateway-restrictions-manager.php
+++ includes/gateways/class-wc-subscriptions-gateway-restrictions-manager.php
@@ -46,6 +46,24 @@ class WC_Subscriptions_Gateway_Restrictions_Manager {
 				'decimal_point_separator'      => $decimal_separator,
 			);
 
+			/**
+			 * Allow WC Payments to return the minimum amount that can be processed.
+			 *
+			 * @since 1.5.0
+			 *
+			 * @param bool|float The minimum amount that can be processed in the given currency.
+			 * @param string.    The currency.
+			 */
+			$minimum_processable_amount = apply_filters( 'woocommerce_subscriptions_minimum_processable_recurring_amount', false, get_woocommerce_currency() );
+
+			if ( $minimum_processable_amount ) {
+				$i18n_minimum_price                         = sprintf( get_woocommerce_price_format(), get_woocommerce_currency_symbol(), number_format( $minimum_processable_amount, $decimals, $decimal_separator, '' ) );
+				$script_data['minimum_subscription_amount'] = $minimum_processable_amount;
+
+				// Translators: The %1 placeholder is the localized minimum price string ($0.50). %2 is the currency identifier string ('USD').
+				$script_data['i18n_below_minimum_subscription_error'] = sprintf( __( 'Warning! Your store\'s payment method cannot process a recurring total below %1$s %2$s.', 'woocommerce-subscriptions' ), $i18n_minimum_price, get_woocommerce_currency() );
+			}
+
 			wp_localize_script( 'woocommerce_subscriptions_payment_restrictions', 'wcs_gateway_restrictions', $script_data );
 		}
 	}
