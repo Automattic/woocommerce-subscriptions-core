a7eeb45f7848f490001281058c766f409de5edcc
mattallan <matt.allan@automattic.com>
2022-04-26 13:59:12 +1000

    Replace update_post_meta(order_id) calls in save_old_paypal_meta()

--- includes/gateways/paypal/includes/class-wcs-paypal-standard-switcher.php
+++ includes/gateways/paypal/includes/class-wcs-paypal-standard-switcher.php
@@ -155,9 +155,10 @@ class WCS_PayPal_Standard_Switcher {
 	 * @since 2.0.15
 	 */
 	public static function save_old_paypal_meta( $order_id, $posted ) {
+		$order = wc_get_order( $order_id );
 
-		if ( wcs_order_contains_switch( $order_id ) ) {
-			$subscriptions = wcs_get_subscriptions_for_order( $order_id, array( 'order_type' => 'switch' ) );
+		if ( wcs_is_order( $order ) && wcs_order_contains_switch( $order ) ) {
+			$subscriptions = wcs_get_subscriptions_for_order( $order, array( 'order_type' => 'switch' ) );
 
 			foreach ( $subscriptions as $subscription ) {
 
@@ -166,8 +167,10 @@ class WCS_PayPal_Standard_Switcher {
 					$paypal_id = wcs_get_paypal_id( $subscription->get_id() );
 
 					if ( ! wcs_is_paypal_profile_a( $paypal_id, 'billing_agreement' ) ) {
-						update_post_meta( $order_id, '_old_payment_method', 'paypal_standard' );
-						update_post_meta( $order_id, '_old_paypal_subscription_id', $paypal_id );
+						$order->update_meta_data( '_old_payment_method', 'paypal_standard' );
+						$order->update_meta_data( '_old_paypal_subscription_id', $paypal_id );
+						$order->save();
+
 						update_post_meta( $subscription->get_id(), '_switched_paypal_subscription_id', $paypal_id );
 					}
 				}
