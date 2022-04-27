88328f80990982f5b1fe8356b1d6f0cc3847f0c2
mattallan <matt.allan@automattic.com>
2022-04-27 10:58:14 +1000

    Add/update PHPUnit tests for wcs_set_objects_property()

--- tests/unit/test-wcs-compatibility-functions.php
+++ tests/unit/test-wcs-compatibility-functions.php
@@ -23,11 +23,20 @@ class WCS_Compatibility_Functions_Test extends WP_UnitTestCase {
 		$this->assertEquals( get_post( $simple_product->get_id() ), wcs_get_objects_property( $simple_product, 'post' ) );
 		$this->assertEquals( get_post( $variation_product->get_parent_id() ), wcs_get_objects_property( $variation_product, 'post' ) );
 
-		// post_status.
+		// post_status - now deprecated as a property
+		$this->expected_deprecated = [
+			'wcs_get_objects_property',
+		];
 		$this->assertEquals( get_post( $subscription->get_id() )->post_status, wcs_get_objects_property( $subscription, 'post_status' ) );
 		$this->assertEquals( get_post( $order->get_id() )->post_status, wcs_get_objects_property( $order, 'post_status' ) );
 		$this->assertEquals( get_post( $simple_product->get_id() )->post_status, wcs_get_objects_property( $simple_product, 'post_status' ) );
 		$this->assertEquals( get_post( $variation_product->get_parent_id() )->post_status, wcs_get_objects_property( $variation_product, 'post_status' ) );
+
+		// status.
+		$this->assertEquals( $subscription->get_status(), wcs_get_objects_property( $subscription, 'status' ) );
+		$this->assertEquals( $order->get_status(), wcs_get_objects_property( $order, 'status' ) );
+		$this->assertEquals( $simple_product->get_status(), wcs_get_objects_property( $simple_product, 'status' ) );
+		$this->assertEquals( $variation_product->get_status(), wcs_get_objects_property( $variation_product, 'status' ) );
 
 		// parent_id.
 		$this->assertEquals( $subscription->get_parent_id(), wcs_get_objects_property( $subscription, 'parent_id' ) );
@@ -105,5 +114,47 @@ class WCS_Compatibility_Functions_Test extends WP_UnitTestCase {
 		$this->assertNull( wcs_get_objects_property( false, 'FAKE_PROPERTY' ) );
 		$this->assertNull( wcs_get_objects_property( '', 'name' ) );
 		$this->assertNull( wcs_get_objects_property( null, 'ID' ) );
+	}
+
+	public function test_wcs_set_objects_property() {
+		$simple_product      = WC_Helper_Product::create_simple_product();
+		$meta_key            = uniqid( 'meta_key_' );
+		$original_name       = 'Original Name';
+		$original_meta_value = 'original_meta_value';
+		$updated_name        = 'Test Product';
+		$updated_meta_value  = 'updated_meta_value';
+
+		$simple_product->set_name( $original_name );
+		$simple_product->update_meta_data( $meta_key, $original_meta_value );
+		$simple_product->save();
+
+		// property without saving
+		wcs_set_objects_property( $simple_product, 'name', $updated_name, 'no save' );
+		$this->assertEquals( $updated_name, $simple_product->get_name() );
+
+		// meta without saving
+		wcs_set_objects_property( $simple_product, $meta_key, $updated_meta_value, 'no save', '', 'no prefix' );
+		$this->assertEquals( $updated_meta_value, $simple_product->get_meta( $meta_key ) );
+
+		// verify reloaded object has original values
+		$reloaded_product = wc_get_product( $simple_product->get_id() );
+		$this->assertEquals( $original_name, $reloaded_product->get_name() );
+		$this->assertEquals( $original_meta_value, $reloaded_product->get_meta( $meta_key ) );
+
+		$simple_product->set_name( $original_name );
+		$simple_product->update_meta_data( $meta_key, $original_meta_value );
+
+		// property with saving
+		wcs_set_objects_property( $simple_product, 'name', $updated_name );
+		$this->assertEquals( $updated_name, $simple_product->get_name() );
+
+		// meta with saving
+		wcs_set_objects_property( $simple_product, $meta_key, $updated_meta_value, 'save', '', 'no prefix' );
+		$this->assertEquals( $updated_meta_value, $simple_product->get_meta( $meta_key ) );
+
+		// verify reloaded object has updated values
+		$reloaded_product = wc_get_product( $simple_product->get_id() );
+		$this->assertEquals( $updated_name, $reloaded_product->get_name() );
+		$this->assertEquals( $updated_meta_value, $reloaded_product->get_meta( $meta_key ) );
 	}
 }
