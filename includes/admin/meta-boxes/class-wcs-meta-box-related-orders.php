6671c9ac2c14c5e8e77f9db58834153a84e268f1
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2022-11-15 15:02:30 +1000

    Update filter deprecated version tag

--- includes/admin/meta-boxes/class-wcs-meta-box-related-orders.php
+++ includes/admin/meta-boxes/class-wcs-meta-box-related-orders.php
@@ -114,14 +114,14 @@ class WCS_Meta_Box_Related_Orders {
 		}
 
 		if ( has_filter( 'woocommerce_subscriptions_admin_related_orders_to_display' ) ) {
-			wcs_deprecated_hook( 'woocommerce_subscriptions_admin_related_orders_to_display', 'subscriptions-core 5.0.0', 'wcs_admin_subscription_related_orders_to_display' );
+			wcs_deprecated_hook( 'woocommerce_subscriptions_admin_related_orders_to_display', 'subscriptions-core 5.1.0', 'wcs_admin_subscription_related_orders_to_display' );
 
 			/**
 			 * Filters the orders to display in the Related Orders meta box.
 			 *
 			 * This filter is deprecated in favour of 'wcs_admin_subscription_related_orders_to_display'.
 			 *
-			 * @deprecated subscriptions-core 5.0.0
+			 * @deprecated subscriptions-core 5.1.0
 			 *
 			 * @param array   $orders_to_display An array of orders to display in the Related Orders meta box.
 			 * @param array   $subscriptions An array of subscriptions related to the order.
@@ -133,7 +133,7 @@ class WCS_Meta_Box_Related_Orders {
 		/**
 		 * Filters the orders to display in the Related Orders meta box.
 		 *
-		 * @since subscriptions-core 5.0.0
+		 * @since subscriptions-core 5.1.0
 		 *
 		 * @param array    $orders_to_display An array of orders to display in the Related Orders meta box.
 		 * @param array    $subscriptions An array of subscriptions related to the order.
