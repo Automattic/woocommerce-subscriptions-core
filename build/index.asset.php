6a26c7f0ff7fc7d7f5ec41c5fe58f2795c238def
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2022-12-15 15:59:01 +1000

    Update `npm run build` artefacts due to dependency updates

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-price-format', 'wc-settings', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '7855adadf2c3ec0499bc9a7854758556');
+<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-price-format', 'wc-settings', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '5a6f67be239fa03255cd');
