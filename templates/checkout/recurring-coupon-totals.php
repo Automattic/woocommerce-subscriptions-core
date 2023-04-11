3d24c9bc327d5c276de73b932d25945becbe25fb
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-04-12 09:39:48 +1000

    Remove @author to fix phpcs violation

--- templates/checkout/recurring-coupon-totals.php
+++ templates/checkout/recurring-coupon-totals.php
@@ -2,7 +2,6 @@
 /**
  * Recurring cart subtotals totals
  *
- * @author  WooCommerce
  * @package WooCommerce Subscriptions/Templates
  * @version 1.0.0 - Migrated from WooCommerce Subscriptions v3.1.0
  */
