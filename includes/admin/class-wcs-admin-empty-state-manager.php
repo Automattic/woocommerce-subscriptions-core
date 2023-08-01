9a1772e224f5e75b5ed708d2afe2fbfbf39dfb87
James Allan <james.allan@automattic.com>
2023-08-02 09:26:58 +1000

    Add function block comments

--- includes/admin/class-wcs-admin-empty-state-manager.php
+++ includes/admin/class-wcs-admin-empty-state-manager.php
@@ -1,6 +1,9 @@
 <?php
 /**
+ * Class WCS_Admin_Empty_State_Manager
  *
+ * @package WooCommerce Subscriptions
+ * @since 6.1.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -8,36 +11,53 @@ defined( 'ABSPATH' ) || exit;
 class WCS_Admin_Empty_State_Manager {
 
 	/**
-	 * Undocumented function
+	 * Initialize the class and attach callbacks.
 	 */
 	public static function init() {
 		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_scripts_and_styles' ) );
 	}
 
 	/**
-	 * Undocumented function
+	 * Gets the Admin Subscriptions list table HTML for the empty state.
 	 *
-	 * @return string
+	 * @return string The HTML for the empty state.
 	 */
 	public static function get_list_table_html() {
-		$description = apply_filters( 'woocommerce_subscriptions_not_found_description', __( "This is where you'll see and manage all subscriptions in your store. Create a subscription product to turn one-time purchases into a steady income.", 'woocommerce-subscriptions' ) );
-		$html        = wc_get_template_html(
+		$html = wc_get_template_html(
 			'html-admin-empty-list-table.php',
-			array( 'description' => $description ),
+			[],
 			'',
 			WC_Subscriptions_Core_Plugin::instance()->get_subscriptions_core_directory( 'templates/admin/' )
 		);
 
+		// Backwards compatibility for the woocommerce_subscriptions_not_found_label filter.
 		if ( has_action( 'woocommerce_subscriptions_not_found_label' ) ) {
-			wcs_deprecated_hook( 'woocommerce_subscriptions_not_found_label', 'x.x.x', 'woocommerce_subscriptions_not_found_html', 'Use the woocommerce_subscriptions_not_found_html filter instead.' );
+			wcs_deprecated_hook( 'woocommerce_subscriptions_not_found_label', '6.1.0', 'woocommerce_subscriptions_not_found_html' );
+
+			/**
+			 * Filters the HTML for the empty state.
+			 *
+			 * The woocommerce_subscriptions_not_found_label filter no longer makes sense as the HTML is now
+			 * more complex - it is no longer just a string. For backwards compatibility we still filter the
+			 * full content shown in the empty state.
+			 *
+			 * @deprecated 6.1.0 Use the woocommerce_subscriptions_not_found_html filter instead.
+			 * @param string $html The HTML for the empty state.
+			 */
 			$html = apply_filters( 'woocommerce_subscriptions_not_found_label', $html );
 		}
 
+		/**
+		 * Filters the HTML for the empty state.
+		 *
+		 * @since 6.1.0
+		 * @param string $html The HTML for the empty state.
+		 */
 		return apply_filters( 'woocommerce_subscriptions_not_found_html', $html );
 	}
 
 	/**
-	 * Undocumented function
+	 * Enqueues the scripts and styles for the empty state.
 	 */
 	public static function enqueue_scripts_and_styles() {
 		$screen = get_current_screen();
