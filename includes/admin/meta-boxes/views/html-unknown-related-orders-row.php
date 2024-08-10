fe04535ff7c61727a4d44d298e1e4e2611095d42
Brian Taylor <45057772+Brianmitchtay@users.noreply.github.com>
2024-08-10 09:59:19 +0700

    Refactor multi-line function call to pass phpcs test

--- includes/admin/meta-boxes/views/html-unknown-related-orders-row.php
+++ includes/admin/meta-boxes/views/html-unknown-related-orders-row.php
@@ -21,7 +21,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<a href="https://woocommerce.com/document/subscriptions/orders/#why-are-some-orders-in-the-related-orders-table-not-linking-to-the-order">
 				<?php
 				// Translators: The %1 placeholder is the translated order relationship ("Parent Order"), %2 placeholder is a <br> HTML tag.
-				echo wcs_help_tip( sprintf( __( 'This %1$s couldn\'t be loaded from the database. %1$s Click to learn more.', 'woocommerce-subscriptions' ), $relationship, '</br>' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				echo wcs_help_tip( 
+					sprintf( 
+						__( 'This %1$s couldn\'t be loaded from the database. %1$s Click to learn more.', 'woocommerce-subscriptions' ),
+						$relationship,
+						'</br>' 
+					) 
+				); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 				?>
 			</a>
 		</div>
