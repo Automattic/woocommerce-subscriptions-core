6e1e87ddd6ed649f508fd0eea3f9b9ab6edc9480
Brian Taylor <45057772+Brianmitchtay@users.noreply.github.com>
2024-08-13 02:03:11 +0700

    move esc_html call & add phpcs ignore comment

--- includes/admin/meta-boxes/views/html-unknown-related-orders-row.php
+++ includes/admin/meta-boxes/views/html-unknown-related-orders-row.php
@@ -19,16 +19,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 		?>
 		<div class="wcs-unknown-order-info-wrapper">
 			<a href="https://woocommerce.com/document/subscriptions/orders/#why-are-some-orders-in-the-related-orders-table-not-linking-to-the-order">
-				<?php
-				echo esc_html(
-					wcs_help_tip(
-						sprintf( // Translators: The %1 placeholder is the translated order relationship ("Parent Order"), %2 placeholder is a <br> HTML tag.
-							__( 'This %1$s couldn\'t be loaded from the database. %1$s Click to learn more.', 'woocommerce-subscriptions' ),
-							$relationship,
-							'</br>'
-						)
+				<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				echo wcs_help_tip(
+					sprintf( // Translators: The %1 placeholder is the translated order relationship ("Parent Order"), %2 placeholder is a <br> HTML tag.
+						__( 'This %1$s couldn\'t be loaded from the database. %1$s Click to learn more.', 'woocommerce-subscriptions' ),
+						esc_html($relationship),
+						'</br>'
 					)
-				); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				);
 				?>
 			</a>
 		</div>
