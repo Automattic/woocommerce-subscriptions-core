ffc202a3f2360182ddf0b5d323dfb601a28a9d94
James Allan <james.allan@automattic.com>
2024-08-26 16:41:29 +1000

    Add more tests for recurring cart calculations

--- tests/unit/test-class-wc-subscriptions-cart.php
+++ tests/unit/test-class-wc-subscriptions-cart.php
@@ -25,13 +25,14 @@ class WC_Subscriptions_Cart_Test extends WP_UnitTestCase {
 		parent::tear_down();
 
 		$this->cart->empty_cart();
+		$this->cart->recurring_carts = [];
 	}
 
 	/**
 	 * Test that recurring carts are created when calculating totals.
 	 */
 	public function test_calculate_subscription_totals() {
-		$product = WCS_Helper_Product::create_simple_subscription_product( array( 'price' => 10 ) );
+		$product = WCS_Helper_Product::create_simple_subscription_product( [ 'price' => 10 ] );
 
 		// First, check that there are no recurring carts.
 		$this->assertEmpty( $this->cart->recurring_carts );
@@ -43,5 +44,64 @@ class WC_Subscriptions_Cart_Test extends WP_UnitTestCase {
 
 		// Check that the recurring cart was created.
 		$this->assertNotEmpty( $this->cart->recurring_carts );
+		$this->assertCount( 1, $this->cart->recurring_carts );
+	}
+
+	/**
+	 * Test that recurring carts are created when calculating totals.
+	 */
+	public function test_calculate_subscription_totals_multiple_recurring_carts() {
+		$product_1 = WCS_Helper_Product::create_simple_subscription_product( [ 'price' => 10 ] );
+		$product_2 = WCS_Helper_Product::create_simple_subscription_product(
+			[
+				'price'               => 20,
+				'subscription_period' => 'year',
+			]
+		);
+
+		// First, check that there are no recurring carts.
+		$this->assertEmpty( $this->cart->recurring_carts );
+
+		$this->cart->add_to_cart( $product_1->get_id() );
+		$this->cart->add_to_cart( $product_2->get_id() );
+
+		// Calculate the totals. This should create a recurring cart.
+		$this->cart->calculate_totals();
+
+		// Check that the recurring cart was created.
+		$this->assertNotEmpty( $this->cart->recurring_carts );
+		$this->assertCount( 2, $this->cart->recurring_carts );
+	}
+
+	/**
+	 * Test that recurring carts are created when calculating totals.
+	 */
+	public function test_calculate_subscription_totals_multiple_items_one_cart() {
+		$product_1 = WCS_Helper_Product::create_simple_subscription_product(
+			[
+				'price'               => 10,
+				'subscription_period' => 'year',
+			]
+		);
+		$product_2 = WCS_Helper_Product::create_simple_subscription_product(
+			[
+				'price'               => 20,
+				'subscription_period' => 'year',
+			]
+		);
+
+		// First, check that there are no recurring carts.
+		$this->assertEmpty( $this->cart->recurring_carts );
+
+		$this->cart->add_to_cart( $product_1->get_id() );
+		$this->cart->add_to_cart( $product_2->get_id() );
+
+		// Calculate the totals. This should create a recurring cart.
+		$this->cart->calculate_totals();
+
+		// Check that the recurring cart was created.
+		$this->assertNotEmpty( $this->cart->recurring_carts );
+		$this->assertCount( 1, $this->cart->recurring_carts ); // Only one recurring cart should be created for both items.
+		$this->assertCount( 2, reset( $this->cart->recurring_carts )->get_cart() );
 	}
 }
