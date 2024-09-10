5149bed5bb89277d961f7d6a999d9454933a20d0
James Allan <james.allan@automattic.com>
2024-09-10 14:56:01 +1000

    Add fullstop

--- includes/class-wc-subscription.php
+++ includes/class-wc-subscription.php
@@ -340,7 +340,7 @@ class WC_Subscription extends WC_Order {
 			case 'completed': // core WC order status mapped internally to avoid exceptions
 			case 'active':
 				if ( $this->payment_method_supports( 'subscription_reactivation' ) && $this->has_status( 'on-hold' ) ) {
-					// If the subscription's end date is in the past, it cannot be reactivated
+					// If the subscription's end date is in the past, it cannot be reactivated.
 					if ( $this->get_time( 'end' ) && $this->get_time( 'end' ) < gmdate( 'U' ) ) {
 						$can_be_updated = false;
 					} else {
