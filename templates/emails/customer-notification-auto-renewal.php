83034b2a00aa87a0f0263821d8e195497a499f89
mattallan <matt.allan@automattic.com>
2024-11-13 17:41:28 +1000

    Remove unnecessary whitespace

--- templates/emails/customer-notification-auto-renewal.php
+++ templates/emails/customer-notification-auto-renewal.php
@@ -57,7 +57,6 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
 // Show subscription details.
 \WC_Subscriptions_Email::subscription_details( $subscription, $order, $sent_to_admin, $plain_text, true );
-
 ?>
 	<p>
 		<small>
