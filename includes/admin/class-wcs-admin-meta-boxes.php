1f5990179c6fa607c6ec9e372be79728d8e2ab26
James Allan <james.allan@automattic.com>
2025-02-12 09:47:34 +1000

    Only fetch subscription object on subscription admin pages (#776)
    * Only fetch subscription object on subscription admin pages
    
    * Add changelog entry
    
    * Avoid unnecessary wcs_is_subscription() call
    
    * Return if the subscription result is no a subscription
    
    * Refine logic for fetching subscription ID from global post ID

--- includes/admin/class-wcs-admin-meta-boxes.php
+++ includes/admin/class-wcs-admin-meta-boxes.php
@@ -136,11 +136,6 @@ class WCS_Admin_Meta_Boxes {
 	public function enqueue_styles_scripts() {
 		global $theorder;
 
-		// If $theorder is empty, fallback to using the global post object.
-		if ( empty( $theorder ) && ! empty( $GLOBALS['post']->ID ) ) {
-			$theorder = wcs_get_subscription( $GLOBALS['post']->ID );
-		}
-
 		// Get admin screen ID.
 		$screen    = get_current_screen();
 		$screen_id = isset( $screen->id ) ? $screen->id : '';
@@ -148,9 +143,22 @@ class WCS_Admin_Meta_Boxes {
 		// Get the script version.
 		$ver = WC_Subscriptions_Core_Plugin::instance()->get_library_version();
 
-		if ( wcs_get_page_screen_id( 'shop_subscription' ) === $screen_id && wcs_is_subscription( $theorder ) ) {
-			// Declare a subscription variable for clearer use. The $theorder global on edit subscription screens is a subscription.
-			$subscription = $theorder;
+		if ( wcs_get_page_screen_id( 'shop_subscription' ) === $screen_id ) {
+			// If global $theorder is empty, fallback to using the global post object.
+			if ( empty( $theorder ) && ! empty( $GLOBALS['post']->ID ) ) {
+				$subscription = wcs_get_subscription( $GLOBALS['post']->ID );
+
+				// If we have a subscription, set it as the global $theorder.
+				if ( $subscription ) {
+					$theorder = $subscription;
+				}
+			} else {
+				$subscription = $theorder;
+			}
+
+			if ( ! wcs_is_subscription( $subscription ) ) {
+				return;
+			}
 
 			wp_register_script( 'jstz', WC_Subscriptions_Core_Plugin::instance()->get_subscriptions_core_directory_url( 'assets/js/admin/jstz.min.js' ), [], $ver, false );
 			wp_register_script( 'momentjs', WC_Subscriptions_Core_Plugin::instance()->get_subscriptions_core_directory_url( 'assets/js/admin/moment.min.js' ), [], $ver, false );
