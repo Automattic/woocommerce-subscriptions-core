8cb2ec16560201cb79831fd762f0dd2e631307b1
James Allan <james.allan@automattic.com>
2025-03-20 11:19:02 +1000

    Prevent ever setting the order awaiting payment ID to a subscription ID

--- includes/class-wcs-cart-renewal.php
+++ includes/class-wcs-cart-renewal.php
@@ -257,14 +257,20 @@ class WCS_Cart_Renewal {
 		if ( is_a( $order_id, 'WC_Abstract_Order' ) ) {
 			$order    = $order_id;
 			$order_id = $order->get_id();
+		} elseif ( ! empty( $order_id ) ) {
+			$order = wc_get_order( $order_id );
+		}
+
+		// Only ever set the order awaiting payment to 0 or an Order ID - not a subscription.
+		if ( $order && ! wcs_is_order( $order ) ) {
+			return;
 		}
 
 		WC()->session->set( 'order_awaiting_payment', $order_id );
 		WC()->session->set( 'store_api_draft_order', $order_id );
 
 		if ( $order_id ) {
-			// To avoid needing to load the order object, pass it if available, otherwise pass the order ID.
-			$this->set_cart_hash( $order ?? $order_id );
+			$this->set_cart_hash( $order );
 		}
 	}
 
