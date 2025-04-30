947fa2f20fb13a28f4241f68f7a7223473cf4c52
barryhughes <3594411+barryhughes@users.noreply.github.com>
2025-04-30 16:35:59 -0700

    Improvements to pagination controls (my account/related orders).

--- templates/myaccount/related-orders.php
+++ templates/myaccount/related-orders.php
@@ -103,31 +103,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</tbody>
 </table>
 
-<div class="woocommerce-subscriptions-related-orders-pagination-links woocommerce-pagination">
-	<?php
-	$prev_page = max( 1, $page - 1 );
-	$next_page = min( $max_num_pages, $page + 1 );
+<?php
+$prev_page = max( 1, $page - 1 );
+$next_page = min( $max_num_pages, $page + 1 );
 
-	$page_link = static function( int $page, string $type, string $label, bool $enabled = true ): string {
-		$base_classes = 'button woocommerce-button wp-element-button';
-		$classes      = esc_attr( $base_classes . ' woocommerce-button--' . $type . ( $enabled ? '' : ' disabled' ) );
-		$url          = esc_url( add_query_arg( 'related_orders_page', $page, '#woocommerce-subscriptions-related-orders-table' ) );
+$page_link = static function( int $page, string $type, string $label, string $alt_symbol, bool $enabled = true ): string {
+	$base_classes = 'button woocommerce-button wp-element-button';
+	$classes      = esc_attr( $base_classes . ' woocommerce-button--' . $type . ( $enabled ? '' : ' disabled' ) );
+	$url          = esc_url( add_query_arg( 'related_orders_page', $page, '#woocommerce-subscriptions-related-orders-table' ) );
+	$label        = esc_html( $label );
+	$aria_label   = esc_attr( $label );
+	$alt_symbol   = esc_html( $alt_symbol );
 
-		return "<a href='$url' class='$classes'>$label</a>";
-	};
+	return "<a href='$url' class='$classes' aria-label='$aria_label'><span class='label'>$label</span><span class='symbol'>$alt_symbol</span></a>";
+};
 
-    // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped -- Our helper function takes care of escaping.
+if ( $max_num_pages > 1 || ( 1 === $max_num_pages && 1 !== $page ) ) :
+
+	// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped -- Our helper function takes care of escaping.
 	?>
-	<div class="pagination-links">
-		<?php echo $page_link( 1, 'first', _x( 'First', 'Related orders pagination', 'woocommerce-subscriptions' ), 1 !== $page ); ?>
-		<?php echo $page_link( $prev_page, 'prev', _x( 'Previous', 'Related orders pagination', 'woocommerce-subscriptions' ), 1 !== $page ); ?>
-	</div>
+		<div class="woocommerce-subscriptions-related-orders-pagination-links woocommerce-pagination">
+			<div class="pagination-links">
+				<?php echo $page_link( 1, 'first', _x( 'First', 'Related orders pagination', 'woocommerce-subscriptions' ), '«', 1 !== $page ); ?>
+				<?php echo $page_link( $prev_page, 'prev', _x( 'Previous', 'Related orders pagination', 'woocommerce-subscriptions' ), '‹', 1 !== $page ); ?>
+			</div>
 
-	<div class="pagination-links">
-		<?php echo $page_link( $next_page, 'next', _x( 'Next', 'Related orders pagination', 'woocommerce-subscriptions' ), $page < $max_num_pages ); ?>
-		<?php echo $page_link( $max_num_pages, 'last', _x( 'Last', 'Related orders pagination', 'woocommerce-subscriptions' ), $page < $max_num_pages ); ?>
-	</div>
-	<?php // phpcs:enable ?>
-</div>
+			<div class="pagination-links">
+				<?php echo $page_link( $next_page, 'next', _x( 'Next', 'Related orders pagination', 'woocommerce-subscriptions' ), '›', $page < $max_num_pages ); ?>
+				<?php echo $page_link( $max_num_pages, 'last', _x( 'Last', 'Related orders pagination', 'woocommerce-subscriptions' ), '»', $page < $max_num_pages ); ?>
+			</div>
+		</div>
+	<?php
+	// phpcs:enable
+endif;
 
-<?php do_action( 'woocommerce_subscription_details_after_subscription_related_orders_table', $subscription ); ?>
+do_action( 'woocommerce_subscription_details_after_subscription_related_orders_table', $subscription );
+?>
