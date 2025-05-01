81c7c2120ecb419ab71e3447a0242c8d84062565
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2025-05-01 14:32:46 -0300

    Reverted comment

--- includes/class-wc-subscriptions-renewal-order.php
+++ includes/class-wc-subscriptions-renewal-order.php
@@ -96,6 +96,7 @@ class WC_Subscriptions_Renewal_Order {
 		$order_needed_payment = in_array( $orders_old_status, apply_filters( 'woocommerce_valid_order_statuses_for_payment', array( 'pending', 'on-hold', 'failed' ), $order ) );
 
 		if ( $order_completed && $order_needed_payment ) {
+			// Prior to WC 3.0, we need to update the post date (i.e. the date created) to have a reliable representation of the paid date (both because it was in GMT and because it was always set). That's not needed in WC 3.0, but some plugins and store owners still rely on it being updated, so we want to make it possible to update it with 3.0 also.
 			if ( apply_filters( 'wcs_renewal_order_payment_update_date_created', false, $order, $subscriptions ) ) {
 				$order->set_date_created( time() );
 				$order->save();
